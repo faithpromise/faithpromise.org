5fc7585a12defe20df80ed15b10e0218d073ccbc
Brad Roberts <bradr@faithpromise.org>
2015-07-07 11:09:32 -0400

    Added nav config

---
+++ config/nav.php
@@ -0,0 +1,89 @@
+<?php
+
+return [
+
+    [
+        'title' => 'About',
+        'subnav' => [
+            [
+                'title' => 'What to Expect',
+                'url' => '/what-to-expect'
+            ],
+            [
+                'title' => 'Meet Our Staff',
+                'url' => '/staff'
+            ],
+            [
+                'title' => 'Beliefs & Values',
+                'url' => '/beliefs-and-values'
+            ],
+            [
+                'title' => 'Heart for the Harvest',
+                'url' => '/h4h'
+            ]
+        ]
+    ],
+    [
+        'title' => 'Locations',
+        'url' => '/locations'
+    ],
+
+    [
+        'title' => 'Ministries',
+        'subnav' => [
+            [
+                'title' => 'Family Ministry',
+                'url' => '/family'
+            ],
+            [
+                'title' => 'Children',
+                'url' => '/fpkids'
+            ],
+            [
+                'title' => 'Students',
+                'url' => '/fpstudents'
+            ],
+            [
+                'title' => 'Groups',
+                'url' => '/groups'
+            ],
+            [
+                'title' => 'Missions',
+                'url' => '/missions'
+            ],
+            [
+                'title' => 'Prayer Ministry',
+                'url' => '/prayer'
+            ],
+            [
+                'title' => 'Worship Ministry',
+                'url' => '/worship'
+            ],
+            [
+                'title' => 'Care Ministries',
+                'url' => '/care'
+            ],
+            [
+                'title' => 'fpCelebrate',
+                'url' => '/celebrate'
+            ]
+        ]
+    ],
+    [
+        'title' => 'Next Steps',
+        'url' => '/nextsteps'
+    ],
+    [
+        'title' => 'Sermons',
+        'url' => '/sermons'
+    ],
+    [
+        'title' => 'Events',
+        'url' => '/events'
+    ],
+    [
+        'title' => 'Give',
+        'url' => '/give'
+    ]
+
+];
