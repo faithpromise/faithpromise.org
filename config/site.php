ff319ca749e0ac776f5b7b5c3b70dca6085bacec
Brad Roberts <bradr@faithpromise.org>
2015-07-07 11:09:25 -0400

    Added site config

---
+++ config/site.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'title'              => 'Faith Promise Church',
+    'description'        => 'A contemporary church with locations in Knoxville, Maryville, Clinton, and La Follette, TN',
+    'url'                => 'http://faithpromise.org',
+    'twitter_username'   => 'faithpromise',
+    'github_username'    => 'faithpromise',
+    'images_root'        => '/build/images',
+    'series_images_root' => '/build/images/series',
+    'staff_images_root'  => '/build/images/staff'
+];
