814acd831d018079f21944e72440ba8d4abb70a1
Brad Roberts <bradr@faithpromise.org>
2015-07-07 17:18:21 -0400

    Added team model

---
+++ app/Team.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Team extends Model
+{
+    use SoftDeletes;
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
+    public function staff() {
+        return $this->belongsToMany('App\Staff')->orderBy('sort');
+    }
+}
