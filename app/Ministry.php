378c9781a23bf5ae63a503943176e5b560166755
Brad Roberts <bradr@faithpromise.org>
2015-07-08 11:38:26 -0400

    Added Ministry model

---
+++ app/Ministry.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Ministry extends Model
+{
+    protected $dates = ['created_at', 'updated_at'];
+
+    public function staff() {
+        return $this->belongsToMany('App\Staff', 'staff_ministry')->orderBy('sort');
+    }
+
+    public function event() {
+        return $this->hasMany('App\Event')->orderBy('sort');
+    }
+}
