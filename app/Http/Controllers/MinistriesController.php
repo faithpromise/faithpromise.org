0c9466f8081e47a2d1c991786db1c8dee231c13c
Brad Roberts <bradr@faithpromise.org>
2015-07-08 11:38:49 -0400

    Added ministry controller

---
+++ app/Http/Controllers/MinistriesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Ministry;
+use Illuminate\Routing\Controller as BaseController;
+
+class MinistriesController extends BaseController
+{
+
+    public function index($ministry_ident)
+    {
+
+        $ministry = Ministry::with('Staff')->with('Event')->where('ident', $ministry_ident)->first();
+
+        return view($ministry_ident, [
+            'staff' => $ministry->staff,
+            'events' => $ministry->event
+        ]);
+    }
+
+}
