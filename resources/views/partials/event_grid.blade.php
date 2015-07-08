96fd6f2ffa4dacab852d4b293572d8108927a0a7
Brad Roberts <bradr@faithpromise.org>
2015-07-08 11:39:44 -0400

    Added event grid partial

---
+++ resources/views/partials/event_grid.blade.php
@@ -0,0 +1,20 @@
+<?php
+
+$css_class = trim('GridSection ' . (isset($class) ? $class : ''));
+$title = isset($title) ? $title : 'Upcoming Events';
+
+?>
+
+<div class="{{ $css_class }}">
+    <div class="GridSection-container">
+        <h2 class="GridSection-title">{{ $title }}</h2>
+        <div class="GridSection-text">{{ isset($text) ? $text : '' }}</div>
+        <ul class="Card-grid" card-grid>
+            @foreach($events as $event)
+                <li class="Card-item">
+                    @include('partials.card', ['title' => $event->title, 'subtitle' => '', 'image' => $event->getThumbnail(), 'text' => $event->excerpt, 'url' => '', 'url_text' => ''])
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</div>
