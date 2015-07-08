f0644fa170a3986d102028c2a291307fd907b10a
Brad Roberts <bradr@faithpromise.org>
2015-07-08 17:35:36 -0400

    Added missionary image url paths to site config

--- config/site.php
+++ config/site.php
@@ -1,14 +1,16 @@
 <?php
 
 return [
-    'title'              => 'Faith Promise Church',
-    'description'        => 'A contemporary church with locations in Knoxville, Maryville, Clinton, and La Follette, TN',
-    'url'                => 'http://faithpromise.org',
-    'email'              => 'info@faithpromise.org',
-    'twitter_username'   => 'faithpromise',
-    'github_username'    => 'faithpromise',
-    'images_root'        => '/build/images',
-    'series_images_root' => '/build/images/series',
-    'staff_images_root'  => '/build/images/staff',
-    'staff_images_dir'   => base_path() . '/public/build/images/staff'
+    'title'                  => 'Faith Promise Church',
+    'description'            => 'A contemporary church with locations in Knoxville, Maryville, Clinton, and La Follette, TN',
+    'url'                    => 'http://faithpromise.org',
+    'email'                  => 'info@faithpromise.org',
+    'twitter_username'       => 'faithpromise',
+    'github_username'        => 'faithpromise',
+    'images_root'            => '/build/images',
+    'series_images_root'     => '/build/images/series',
+    'staff_images_root'      => '/build/images/staff',
+    'staff_images_dir'       => base_path() . '/public/build/images/staff',
+    'missionary_images_root' => '/build/images/missionaries',
+    'missionary_images_dir'  => base_path() . '/public/build/images/missionaries'
 ];
