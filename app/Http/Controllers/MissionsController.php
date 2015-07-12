5dcfc14206ada0209c0e0b75ef42881b45ad6684
Brad Roberts <email@bradroberts.net>
2015-07-12 15:08:36 -0400

    Refactored missions controller now that we have expire trait working globally

--- app/Http/Controllers/MissionsController.php
+++ app/Http/Controllers/MissionsController.php
@@ -8,7 +8,6 @@ use App\Ministry;
 use App\Missionary;
 use App\MissionTrip;
 use App\MissionLocation;
-use \Carbon\Carbon;
 use Illuminate\Routing\Controller as BaseController;
 
 class MissionsController extends BaseController
@@ -16,24 +15,20 @@ class MissionsController extends BaseController
 
     public function index()
     {
-        $ministry_ident = 'missions';
-        $ministry = Ministry::whereIdent($ministry_ident)->with('Staff')->first();
-        $events = Event::where('ministry_id', '=', $ministry->id)->where('expire_at', '>', Carbon::now())->get();
+        $ministry = Ministry::whereIdent('missions')->first();
+        $staff = $ministry->staff;
+        $events = Event::where('ministry_id', '=', $ministry->id)->get();
         $locations = MissionLocation::upcoming()->get();
         $missionaries = Missionary::with('missionlocation')->get();
-//        dd($locations);
 
-        return view('missions', ['events' => $events, 'missionaries' => $missionaries, 'staff' => $ministry->Staff, 'locations' => $locations]);
-//        return view('missions_2', ['locations' => $locations]);
+        return view('missions', ['events' => $events, 'missionaries' => $missionaries, 'staff' => $staff, 'locations' => $locations]);
     }
 
     public function location($location_ident)
     {
 
         $location = MissionLocation::whereIdent($location_ident)->first();
-        $trips = MissionTrip::where('missionlocation_id', '=', $location->id)->where(function ($query) {
-            $query->whereNull('expire_at')->orWhere('expire_at', '>', Carbon::now());
-        })->get();
+        $trips = MissionTrip::where('missionlocation_id', '=', $location->id)->get();
         dd($past_trips->toArray());
 
         return view('missions_location', ['location' => $location]);
