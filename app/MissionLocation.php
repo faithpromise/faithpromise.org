7862d4aa51bd0cfff781352f5d62e01e13201c30
Brad Roberts <email@bradroberts.net>
2015-07-11 22:38:20 -0400

    Added "upcoming" scope

--- app/MissionLocation.php
+++ app/MissionLocation.php
@@ -2,6 +2,8 @@
 
 namespace App;
 
+use DB;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class MissionLocation extends Model
@@ -9,15 +11,32 @@ class MissionLocation extends Model
 
     protected $dates = ['created_at', 'updated_at'];
 
-    public function missionary() {
+    public function getUrlAttribute()
+    {
+        return '/missions/' . $this->ident;
+    }
+
+    public function missionaries()
+    {
         return $this->hasMany('App\Missionary');
     }
 
-    public function mission_trip() {
+    public function missionTrips()
+    {
         return $this->hasMany('App\MissionTrip');
     }
 
-    public function getUrl() {
-        return '/missions/' . $this->ident;
+    public function scopeUpcoming($query)
+    {
+        $query->where('is_continual', '=', 1)
+            ->orWhereRaw(DB::raw('id in (select mission_location_id from mission_trips where ends_at > NOW() or ends_at IS NULL)'))
+            ->with(['missiontrips' => function ($query) {
+                $query->where(function ($query) {
+                    $query
+                        ->whereNull('ends_at')
+                        ->orWhere('ends_at', '>', Carbon::now());
+                });
+            }]);
     }
 }
+
