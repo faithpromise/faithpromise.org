3b98d69b2a3bbf0d05f9973db6d02109d524ce04
Brad Roberts <email@bradroberts.net>
2015-07-11 22:41:43 -0400

    Added "isHappeningNow" attribute

--- app/MissionTrip.php
+++ app/MissionTrip.php
@@ -2,11 +2,20 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class MissionTrip extends Model
 {
-    protected $dates = ['expire_at', 'created_at', 'updated_at'];
+    protected $dates = ['starts_at', 'ends_at', 'expire_at', 'created_at', 'updated_at'];
+
+    public function getIsHappeningNowAttribute() {
+        return
+            !is_null($this->starts_at)
+            && $this->starts_at->isPast()
+            && !is_null($this->ends_at)
+            && $this->ends_at->isFuture();
+    }
 
     public function missionlocation() {
         return $this->belongsTo('App\MissionLocation');
