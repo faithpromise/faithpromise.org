67d73449eba6723b04b06a68800ad204a0db24fc
Brad Roberts <email@bradroberts.net>
2015-07-11 22:44:42 -0400

    Refactored missions controller

--- app/Http/Controllers/MissionsController.php
+++ app/Http/Controllers/MissionsController.php
@@ -16,40 +16,15 @@ class MissionsController extends BaseController
 
     public function index()
     {
-
-        $trips = MissionLocation::load(['MissionTrip' => function($query) {
-            $query->where('title', 'like', '%foo%');
-//            dd($query->toSql());
-        }])->where(function($query) {
-//            dd($query->toSql());
-        })->get();
-
-        dd($trips);
-
-        $sql = <<<SQL
-            SELECT l.*, t.id, t.dates
-            FROM mission_locations l
-                LEFT JOIN mission_trips t ON t.id = (
-                    SELECT tt.missionlocation_id
-                    FROM mission_trips tt
-                    WHERE tt.missionlocation_id = l.id
-                    ORDER BY tt.expire_at DESC limit 1
-                )
-            WHERE (t.id IS NOT NULL) OR (l.is_continual = 1)
-            ORDER BY l.id
-SQL;
-
-        $trips = DB::select($sql);
-
-        dd($trips);
-
         $ministry_ident = 'missions';
         $ministry = Ministry::whereIdent($ministry_ident)->with('Staff')->first();
         $events = Event::where('ministry_id', '=', $ministry->id)->where('expire_at', '>', Carbon::now())->get();
-        $trips = MissionTrip::with('MissionLocation')->whereNull('expire_at')->orWhere('expire_at', '>', Carbon::now())->get();
-        $missionaries = Missionary::with('MissionLocation')->get();
+        $locations = MissionLocation::upcoming()->get();
+        $missionaries = Missionary::with('missionlocation')->get();
+//        dd($locations);
 
-        return view('missions', ['events' => $events, 'missionaries' => $missionaries, 'staff' => $ministry->staff, 'trips' => $trips]);
+        return view('missions', ['events' => $events, 'missionaries' => $missionaries, 'staff' => $ministry->Staff, 'locations' => $locations]);
+//        return view('missions_2', ['locations' => $locations]);
     }
 
     public function location($location_ident)
