f3b09bb6ead84d4c9dc749f23c185b7d6c86c104
Brad Roberts <bradr@faithpromise.org>
2015-07-14 09:47:57 -0400

    Updated fpstudents page

--- resources/views/fpstudents.blade.php
+++ resources/views/fpstudents.blade.php
@@ -1,7 +1,23 @@
----
-layout: ministry
-permalink: /students/
-title: Students
----
-
 <!--TODO: Add page content-->
+
+
+@extends('layouts.page', ['title' => 'fpStudents'])
+
+@section('page')
+
+    <div class="TextSection">
+        <div class="TextSection-container">
+            <h2 class="TextSection-title">Welcome to fpStudents</h2>
+
+            <div class="TextSection-text">
+                <p>fpStudents is the connection point for 6th-12th grade students at Faith Promise Church. Join us each Wednesday night for a worship service like no other! Loaded with exciting videos, live music, new friendships, and relevant teaching that connects the Bible to your life. fpStudents is a can't-miss experience that will kick-start your world!</p>
+                <p>Check out what's going on this week and in the future at <a href="http://fpStudents.com">fpStudents.com</a>!</p>
+            </div>
+        </div>
+    </div>
+
+    @include('partials.event_grid', ['events' => $events, 'class' => 'Section--lightGrey'])
+
+    @include('partials.staff_gallery', ['staff' => $staff, 'title' => 'Meet the fpStudents Staff'])
+
+@endsection
