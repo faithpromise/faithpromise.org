dc0a2d6bc1f463c86b2d3ded2c7ae1d6899ee334
Brad Roberts <bradr@faithpromise.org>
2015-07-14 17:02:11 -0400

    Updated women's page

--- resources/views/women.blade.php
+++ resources/views/women.blade.php
@@ -1,7 +1,42 @@
----
-layout: ministry
-permalink: /women/
-title: Women\'s Groups
----
+@extends('layouts.page', ['title' => 'Women\'s Groups'])
 
-<!--TODO: Add page content-->
+<!-- TODO: Needs hero image -->
+
+@section('page')
+
+    <div class="TextSection">
+        <div class="TextSection-container">
+            <h2 class="TextSection-title">Women's Ministry</h2>
+
+            <div class="TextSection-text">
+                <p>fp Women's Groups exist to help women become who God created them to be in order that they can fulfill their destiny. We will equip and encourage women to connect with other women, grow into a deeper relationship with Jesus Christ, and serve one another, our community, and the world.</p>
+
+                <blockquote>
+                    "Therefore, as you received Christ Jesus the Lord, so walk in Him, rooted and built up in Him and established in the faith, just as you were taught, abounding in thanksgiving." - Colossians 2:6-7
+                </blockquote>
+            </div>
+        </div>
+    </div>
+
+    <div class="TextSection Section--lightGrey">
+        <div class="TextSection-container">
+            <h2 class="TextSection-title">Women's Conference</h2>
+
+            <div class="TextSection-text">
+                <p>The 2015 Bold Conference will be held on Friday, September 18, in the Worship Center at the Pellissippi Campus. We will experience an incredible time as we learn more about the ROAR - the BOLDNESS we have in Christ. More details to come as the event draws nearer.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="TextSection">
+        <div class="TextSection-container">
+            <h2 class="TextSection-title">Bible Studies</h2>
+
+            <div class="TextSection-text">
+                <p>Spring Semester Bible Studies begin on April 8 at 10:00 am and April 9 at 6:30 pm at the Pellissippi Campus.</p>
+                <p class="text-center"><a class="Button" href="https://fpctystn.infellowship.com/GroupSearch/Show?zipcode=&category=7077&weekday=&start_time=">Find a Group or Bible Study</a></p>
+            </div>
+        </div>
+    </div>
+
+@endsection
