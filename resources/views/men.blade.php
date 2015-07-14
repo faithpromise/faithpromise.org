90b3e1c91df3a4427c0bb384f2ab9ee5ec902612
Brad Roberts <bradr@faithpromise.org>
2015-07-14 16:50:16 -0400

    Updated men's page

--- resources/views/men.blade.php
+++ resources/views/men.blade.php
@@ -1,7 +1,22 @@
----
-layout: ministry
-permalink: /men/
-title: Men\'s Groups
----
-
 <!--TODO: Add page content-->
+
+@extends('layouts.page', ['title' => 'Men\'s Groups'])
+
+@section('page')
+
+    <div class="TextSection">
+        <div class="TextSection-container">
+            <h2 class="TextSection-title">Men's Ministry</h2>
+
+            <div class="TextSection-text">
+                <p>Men, are you experiencing the growth, significance, adventure, purpose, and blessings God has for you? fpMen helps us in the journey to be a man of God in today's world as we connect, serve our community, grow spiritually, and win the battle for our character, families, and legacy.</p>
+                <p>For every ten men in the United States, nine will have kids who leave the church, eight will have dissatisfying jobs, seven will look at pornography, six will pay only the monthly minimum on their credit card, four will get divorced, and only one will have a biblical worldview. Our men's groups exist to change these statistics. By connecting, growing, and serving, you can become the husband, father, and spiritual leader God designed you to be.</p>
+                <!-- TODO: change date --><p>In 2013, our focus is on connecting men in groups as we learn practical ways to be a man of God in a fallen world.</p>
+                <p>Growth happens in groups. You may find that your monthly table group fits your needs and schedule, or you may choose to find a group that meets more frequently.</p>
+                <p class="Footnote">*stats from No Man Left Behind by Pat Morley</p>
+                <p class="text-center"><a class="Button" href="Men, are you experiencing the growth, significance, adventure, purpose, and blessings God has for you? fpMen helps us in the journey to be a man of God in today's world as we connect, serve our community, grow spiritually, and win the battle for our character, families, and legacy.">Find a Men's Group</a></p>
+            </div>
+        </div>
+    </div>
+
+@endsection
