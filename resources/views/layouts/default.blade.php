6a37ba5c2f48439d7a763a0621ad8d7d6b78f280
Brad Roberts <bradr@faithpromise.org>
2015-07-14 16:31:38 -0400

    Added image lazy loading

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -105,6 +105,14 @@
         <script src="/build/js/main.dev.js"></script>
         <!-- /build -->
 
+        <script src="//cdnjs.cloudflare.com/ajax/libs/layzr.js/1.4.2/layzr.min.js"></script>
+        <script>
+            var fp = window.fp || {};
+            /*! contentloaded.min.js - https://github.com/dperini/ContentLoaded - Author: Diego Perini - License: MIT */
+            fp.contentLoaded = function(b,i){var j=false,k=true,a=b.document,l=a.documentElement,f=a.addEventListener,h=f?'addEventListener':'attachEvent',n=f?'removeEventListener':'detachEvent',g=f?'':'on',c=function(d){if(d.type=='readystatechange'&&a.readyState!='complete')return;(d.type=='load'?b:a)[n](g+d.type,c,false);if(!j&&(j=true))i.call(b,d.type||d)},m=function(){try{l.doScroll('left')}catch(e){setTimeout(m,50);return}c('poll')};if(a.readyState=='complete')i.call(b,'lazy');else{if(!f&&l.doScroll){try{k=!b.frameElement}catch(e){}if(k)m()}a[h](g+'DOMContentLoaded',c,false);a[h](g+'readystatechange',c,false);b[h](g+'load',c,false)}};
+            fp.contentLoaded(window, function() { new Layzr(); });
+        </script>
+
         <!--[if gte IE 9]>
         <script src="/build/js/svg4everybody.min.js"></script>
         <![endif]-->
