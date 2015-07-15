e63f6fe9b5568668cb6e1f7a2fc9a5caa1e4f246
Brad Roberts <bradr@faithpromise.org>
2015-07-15 17:42:09 -0400

    Updated events table migration

--- database/migrations/2015_07_06_145602_create_events_table.php
+++ database/migrations/2015_07_06_145602_create_events_table.php
@@ -15,6 +15,7 @@ class CreateEventsTable extends Migration
         Schema::create('events', function (Blueprint $table) {
             $table->increments('id');
             $table->integer('ministry_id')->nullable();
+            $table->integer('calendar_event_number')->nullable();
             $table->string('title', 50);
             $table->string('dates', 35)->nullable();
             $table->string('image', 35)->nullable();
