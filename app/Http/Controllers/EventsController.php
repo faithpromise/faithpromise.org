392004d316e16c2d5a42fbd997bd00a4ca04d968
Brad Roberts <bradr@faithpromise.org>
2015-07-15 17:43:26 -0400

    Updated events controller

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -8,7 +8,9 @@
 
 namespace App\Http\Controllers;
 
+use App\CalendarEvent;
 use App\Event;
+use Carbon\Carbon;
 use Illuminate\Routing\Controller as BaseController;
 
 class EventsController extends BaseController
@@ -20,6 +22,37 @@ class EventsController extends BaseController
 
         return view('events', [
             'events' => $events
+        ]);
+    }
+
+    public function calendar()
+    {
+        $year = Carbon::now()->year;
+        $month = Carbon::now()->month;
+
+        return $this->calendarMonth($year, $month);
+    }
+
+    public function calendarMonth($year, $month)
+    {
+
+        $beginningOfMonth = Carbon::create($year, $month, 1);
+        $endOfMonth = $beginningOfMonth->copy()->endOfMonth();
+
+        // TODO: Remove withPast after figuring out how to make it work
+        $days = CalendarEvent::withPast()
+            ->where('starts_at', '>', $beginningOfMonth)
+            ->where('ends_at', '<', $endOfMonth)
+            ->orderBy('starts_at')
+            ->get()
+            ->groupBy(function ($event) {
+                return $event->starts_at->format('Y-m-d');
+            });
+
+        return view('events_calendar', [
+            'start' => $beginningOfMonth,
+            'end'   => $endOfMonth,
+            'days'  => $days
         ]);
     }
 
