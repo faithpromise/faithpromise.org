3a8a8c4c6fb09f51f46c2fdd6b197a23cc642780
Brad Roberts <bradr@faithpromise.org>
2015-07-16 15:57:08 -0400

    Added a `bgsection` blade directive to dry up the html

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,19 +2,38 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
-    {
+    public function boot() {
+
         view()->share('site', config('site'));
         view()->share('nav', config('nav'));
+
+        // BackgroundSection
+        Blade::directive('bgsection', function ($expression) {
+            return '
+                <?php
+                  $bg_section = array_merge(["title" => "", "class" => "", "image" => ""], ' . $expression . ');
+                  $bg_section["image_class"] = "' . uniqid('bg_') . '";
+                ?>
+                <style type="text/css" scoped>.<?= $bg_section["image_class"]; ?> { background-image: url(<?= $bg_section["image"]; ?>); }</style>
+                <div class="BackgroundSection <?= $bg_section["image_class"] . " " . $bg_section["class"]; ?>">
+                    <div class="BackgroundSection-container">
+                        <h2 class="BackgroundSection-title"><?= $bg_section["title"]; ?></h2>
+                        <div class="BackgroundSection-text">
+            ';
+        });
+
+        Blade::directive('endbgsection', function () {
+            return '</div></div></div>';
+        });
     }
 
     /**
@@ -22,8 +41,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
-    {
+    public function register() {
         //
     }
 }
