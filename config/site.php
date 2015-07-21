a0af077487c6e118fcd93886d44b4b018ae339da
Brad Roberts <bradr@faithpromise.org>
2015-07-21 14:28:57 -0400

    Removed unneeded config vars

--- config/site.php
+++ config/site.php
@@ -7,10 +7,5 @@ return [
     'email'                  => 'info@faithpromise.org',
     'twitter_username'       => 'faithpromise',
     'github_username'        => 'faithpromise',
-    'images_root'            => '/build/images',
-    'series_images_root'     => '/build/images/series',
-    'staff_images_root'      => '/build/images/staff',
-    'staff_images_dir'       => base_path() . '/public/build/images/staff',
-    'missionary_images_root' => '/build/images/missions/missionaries',
-    'missionary_images_dir'  => base_path() . '/public/build/images/missions/missionaries'
+    'cdn_url'                => env('CDN_URL', '')
 ];
