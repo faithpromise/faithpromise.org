b6683ef639d139511e79b540702c1a975a25c1e0
Brad Roberts <bradr@faithpromise.org>
2015-07-21 17:02:46 -0400

    Added hero image url helper

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -24,11 +24,14 @@ function card_image_url($path) {
 }
 
 function album_image_url($path) {
-
     $src = base_path($path);
     if (file_exists($src)) {
         return config('site.cdn_url') . route('album_image', ['path' => $path, 'version' => hash_file('md5', $src)], false);
     }
-    return 'https://randomuser.me/api/portraits/lego/' . rand(0, 9) .'.jpg';
+    return '';
+}
 
+function hero_image_url($path) {
+    $src = base_path($path);
+    return config('site.cdn_url') . route('hero_image', ['path' => $path, 'version' => hash_file('md5', $src)], false);
 }
