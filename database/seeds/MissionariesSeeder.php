53847db4f7e3ce8826b8b0d78178e2cd0a9f1ccf
Brad Roberts <bradr@faithpromise.org>
2015-07-21 14:29:38 -0400

    Added an ident column to missionary table and removed image column

--- database/seeds/MissionariesSeeder.php
+++ database/seeds/MissionariesSeeder.php
@@ -4,51 +4,49 @@ use App\MissionLocation;
 use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 
-class MissionariesSeeder extends Seeder
-{
+class MissionariesSeeder extends Seeder {
 
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run()
-    {
+    public function run() {
 
         DB::table('missionaries')->truncate();
 
         DB::table('missionaries')->insert([
+            'ident'               => 'beukemas',
             'name'                => 'The Beukemas',
             'mission_location_id' => MissionLocation::where('ident', '=', 'jamaica')->first()->id,
             'url'                 => 'http://bkbeukema.org',
-            'image'               => 'beukemas-square.jpg',
             'created_at'          => Carbon::now(),
             'updated_at'          => Carbon::now()
         ]);
 
         DB::table('missionaries')->insert([
+            'ident'               => 'coplands',
             'name'                => 'The Coplands',
             'mission_location_id' => MissionLocation::where('ident', '=', 'italy')->first()->id,
             'url'                 => 'http://nickandshannan.org',
-            'image'               => 'coplands-square.jpg',
             'created_at'          => Carbon::now(),
             'updated_at'          => Carbon::now()
         ]);
 
         DB::table('missionaries')->insert([
+            'ident'               => 'chris-ladd',
             'name'                => 'Chris Ladd',
             'mission_location_id' => MissionLocation::where('ident', '=', 'south-africa')->first()->id,
             'url'                 => 'http://chrisleeladd.com',
-            'image'               => 'chris-ladd-square.jpg',
             'created_at'          => Carbon::now(),
             'updated_at'          => Carbon::now()
         ]);
 
         DB::table('missionaries')->insert([
+            'ident'               => 'julie-rumph',
             'name'                => 'Julie Rumph',
             'mission_location_id' => MissionLocation::where('ident', '=', 'south-africa')->first()->id,
             'url'                 => 'http://julierumph.org',
-            'image'               => 'julie-rumph-square.jpg',
             'created_at'          => Carbon::now(),
             'updated_at'          => Carbon::now()
         ]);
