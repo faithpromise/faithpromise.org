3be82e1b3604d3b37fbfd43b2970c2d703682915
Brad Roberts <brad@bradroberts.net>
2015-07-22 18:34:18 -0400

    Added a helper method to get the series fadeout image url

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -18,3 +18,10 @@ function cdn_image($display_width, $image_width, $image_path) {
     $img_url = config('site.cdn_url') . '/' . $display_width . '/' . $image_width . '/' . $image_path . $img_hash;
     return $img_url;
 }
+
+function series_fade_image($series_ident) {
+    $image_path = asset_path('images/series/' . $series_ident . '.jpg');
+    $img_hash = asset_exists($image_path) ? '?v=' . filemtime(asset_path($image_path)) : '';
+    $img_url = config('site.cdn_url') . '/series-fade-out/' . $series_ident . '.jpg' . $img_hash;
+    return $img_url;
+}
