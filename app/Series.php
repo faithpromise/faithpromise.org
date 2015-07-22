22cc48f8f94380cd290825f82f4436495a0c56be
Brad Roberts <brad@bradroberts.net>
2015-07-22 17:45:54 -0400

    Added method on Series model to get a humanized date

--- app/Series.php
+++ app/Series.php
@@ -2,6 +2,7 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use App\PublishedTrait;
 
@@ -24,13 +25,40 @@ class Series extends Model {
     }
 
     public function getWhenAttribute() {
-        return $this->starts_at->format('b Y');
+        return $this->starts_at->format('M Y');
+    }
 
-//{% if item.starts_on > site.time or latest_sermon_series.ident == item.ident %}
-//<series-date starts="{{ item.starts_on }}" next-series-starts="{{ next_series_starts }}"></series-date>
-//{% else %}
-//{{ $this->starts_at->format('b Y') }}
-//{% endif %}
+    public function getDate($next_series_starts) {
+
+        $today = Carbon::today();
+        $diff_in_days = $today->diffInDays($this->starts_at, false);
+
+        // Next series has already started, just show the date for this series
+        if (!is_null($next_series_starts) AND $next_series_starts->gt($today)) {
+            return $this->starts_at->format('M, Y');
+        }
+
+        // Series started before today
+        if ($diff_in_days < 0) {
+            return 'Current series';
+        }
+
+        // Series started today
+        if ($diff_in_days === 0) {
+            return 'Begins today!';
+        }
+
+        // 6 days away (it's Sunday before it starts (on Saturday)
+        if ($diff_in_days === 6) {
+            return 'Begins in 1 week';
+        }
+
+        // More than a week away
+        if ($diff_in_days >= 7) {
+            return 'Begins in ' . $this->starts_at->diffInWeeks($today) . ' week' . ($this->starts_at->diffInWeeks($today) > 1 ? 's' : '');
+        }
+
+        return 'Begins this weekend';
     }
 
 }
