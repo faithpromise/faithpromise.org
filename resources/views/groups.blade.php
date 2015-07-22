480118aa733a5efc8f60eb01945fa3f458950864
Brad Roberts <brad@bradroberts.net>
2015-07-22 17:54:21 -0400

    Fixed image paths

--- resources/views/groups.blade.php
+++ resources/views/groups.blade.php
@@ -5,35 +5,35 @@ $men->card_title = 'Men\'s Groups';
 $men->card_text = 'Connect with other men for service or spiritual growth.';
 $men->card_url = '/men';
 $men->card_url_text = 'Learn more';
-$men->card_image = 'images/hero/men-tall.jpg';
+$men->card_image = 'images/pages/men-tall.jpg';
 
 $women = new stdClass();
 $women->card_title = 'Women\'s Groups';
 $women->card_text = 'Join with other women as you grow in Christ.';
 $women->card_url = '/women';
 $women->card_url_text = 'Learn more';
-$women->card_image = 'images/hero/worship-tall.jpg';
+$women->card_image = 'images/pages/women-tall.jpg'; // TODO: Need this image
 
 $young = new stdClass();
 $young->card_title = 'Young Adult Groups';
 $young->card_text = 'Connect with other 18-30 year old adults.';
 $young->card_url = '/youngadults';
 $young->card_url_text = 'Learn more';
-$young->card_image = 'images/hero/youngadults-tall.jpg';
+$young->card_image = 'images/pages/youngadults-tall.jpg';
 
 $support = new stdClass();
 $support->card_title = 'Support Groups';
 $support->card_text = 'fp Celebrate meets on Monday nights at the FPC Pellissippi Campus starting at 6:00 pm and is a;time of helping people connect, heal, and grow.';
 $support->card_url = '/celebrate';
 $support->card_url_text = 'Learn more';
-$support->card_image = 'images/hero/celebrate-tall.jpg';
+$support->card_image = 'images/pages/celebrate-tall.jpg';
 
 $starting = new stdClass();
 $starting->card_title = 'Starting Point Groups';
 $starting->card_text = 'Starting Point helps participants explore the Bible and begin to understand key truths of the Christian faith.';
 $starting->card_url = '/starting-point';
 $starting->card_url_text = 'Learn more';
-$starting->card_image = 'images/hero/worship-tall.jpg';
+$starting->card_image = 'images/pages/worship-tall.jpg';
 
 $group_types = collect([$men, $women, $young, $support, $starting]);
 
