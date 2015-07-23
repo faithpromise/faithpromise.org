047898cfc76ab8d9df38e877f627c197e4631ed7
Brad Roberts <brad@bradroberts.net>
2015-07-23 09:23:08 -0400

    Updated site config

--- config/site.php
+++ config/site.php
@@ -1,13 +1,19 @@
 <?php
 
+use Illuminate\Support\Facades\App;
+
 return [
-    'title'            => 'Faith Promise Church',
-    'description'      => 'A contemporary church with locations in Knoxville, Maryville, Clinton, and La Follette, TN',
-    'url'              => 'http://faithpromise.org',
-    'email'            => 'info@faithpromise.org',
-    'twitter_username' => 'faithpromise',
-    'github_username'  => 'faithpromise',
-    'audio_url'        => 'http://fpcresources.org/audio/',
-    'cdn_url'          => env('CDN_URL', '//assets.faithpromise.192.168.10.10.xip.io'),
-    'assets_path'      => env('ASSETS_PATH', base_path('../assets.faithpromise.org/public'))
+    'title'              => 'Faith Promise Church',
+    'description'        => 'A contemporary church with locations in Knoxville, Maryville, Clinton, and La Follette, TN',
+    'url'                => 'http://faithpromise.org',
+    'email'              => 'info@faithpromise.org',
+    'facebook_username'  => 'faithpromise',
+    'instagram_username' => 'faithpromise',
+    'twitter_username'   => 'faithpromise',
+    'pinterest_username' => 'faithpromise',
+    'github_username'    => 'faithpromise',
+    'facebook_app_id'    => App::environment('production') ? '1592675454329086' : '1592691894327442',
+    'audio_url'          => 'http://fpcresources.org/audio/',
+    'cdn_url'            => App::environment('local') ? '//assets.faithpromise.192.168.10.10.xip.io' : 'd3m6gouty6q7nm.cloudfront.net',
+    'assets_path'        => env('ASSETS_PATH', base_path('../assets.faithpromise.org/public'))
 ];
