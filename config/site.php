a8ccd8b3f262f2e83cb4009bdf98d5dd80b9f33c
Brad Roberts <brad@bradroberts.net>
2015-07-24 09:59:04 -0400

    Cloud front path fix

--- config/site.php
+++ config/site.php
@@ -12,6 +12,6 @@ return [
     'github_username'    => 'faithpromise',
     'facebook_app_id'    => (env('APP_ENV', 'local') === 'production') ? '1592675454329086' : '1592691894327442',
     'audio_url'          => 'http://fpcresources.org/audio/',
-    'cdn_url'            => (env('APP_ENV', 'local') === 'local') ? '//assets.faithpromise.192.168.10.10.xip.io' : 'd3m6gouty6q7nm.cloudfront.net',
+    'cdn_url'            => (env('APP_ENV', 'local') === 'local') ? '//assets.faithpromise.192.168.10.10.xip.io' : '//d3m6gouty6q7nm.cloudfront.net',
     'assets_path'        => env('ASSETS_PATH', base_path('../assets.faithpromise.org/public'))
 ];
