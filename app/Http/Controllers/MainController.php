de49c2b1374095fd92e23d693cebda31dc68387f
Brad Roberts <brad@bradroberts.net>
2015-07-27 11:31:08 -0400

    Cleanup

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -1,14 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: broberts
- * Date: 7/7/15
- * Time: 2:01 PM
- */
 
 namespace App\Http\Controllers;
 
-use App\Ministry;
 use App\Team;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Route;
