5e2a7cd0e2e3db44840d6acd2ceac2a6cdf93a20
Brad Roberts <brad@bradroberts.net>
2015-07-28 16:18:02 -0400

    Text fix on locations page

--- resources/views/locations.blade.php
+++ resources/views/locations.blade.php
@@ -8,7 +8,7 @@
 
 @section('page')
 
-    @cardsection(['title' => 'Find a Location', 'cards' => $campuses])
+    @cardsection(['title' => 'Find a Location', 'cards' => $campuses, 'class' => 'GridSection--center'])
     <!-- TODO: Text is not centered. Center all cardsection text? Search for occurrences -->
     <p>Join us this weekend at a location near you.</p>
     @endcardsection
