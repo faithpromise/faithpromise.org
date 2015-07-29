83b7f79a88f0c0a62b8aca1309ad5ffb34d6de29
Brad Roberts <brad@bradroberts.net>
2015-07-29 12:28:53 -0400

    Lots of work on the locations page

--- app/Http/Controllers/CampusesController.php
+++ app/Http/Controllers/CampusesController.php
@@ -11,8 +11,30 @@ class CampusesController extends BaseController {
 
         $campuses = Campus::all();
 
+        $campus_obj = [];
+        foreach($campuses as $campus) {
+            $campus_obj[$campus->ident] = [
+                'ident' => $campus->ident,
+                'name' => $campus->name,
+                'title' => $campus->name . ' Campus',
+                'location' => $campus->location,
+                'address' => $campus->address,
+                'city' => $campus->city,
+                'state' => $campus->state,
+                'zip' => $campus->zip,
+                'lat' => $campus->lat,
+                'lng' => $campus->lng,
+                'map_url' => $campus->map_url,
+                'directions_url' => $campus->directions_url,
+                'thumbnail' => cdn_image('sm', 'full', $campus->thumbnail),
+                'url' => $campus->url,
+                'times' => json_decode($campus->times)
+            ];
+        }
+
         return view('locations', [
-            'campuses' => $campuses
+            'campuses' => $campuses,
+            'campuses_json' => json_encode($campus_obj)
         ]);
     }
 
--- resources/views/locations.blade.php
+++ resources/views/locations.blade.php
@@ -1,10 +1,8 @@
 <?php
-// IMAGE: Need hero image for staff
-// IMAGE: add location photos
-// PAGE: Add content
+// IMAGE: Need hero image
 ?>
 
-@extends('layouts.page', ['title' => 'Locations', 'hero-image' => 'images/locations/pellissippi-wide.jpg'])
+@extends('layouts.page', ['title' => 'Locations'])
 
 @section('page')
 
@@ -12,5 +10,157 @@
     <!-- TODO: Text is not centered. Center all cardsection text? Search for occurrences -->
     <p>Join us this weekend at a location near you.</p>
     @endcardsection
+
+    <div id="LocationsMap" class="LocationsMap"></div>
+
+    <script>
+        window.fp = window.fp || {};
+        window.fp.locations = <?= $campuses_json ?>;
+    </script>
+
+    <script type="text/javascript" src="//maps.googleapis.com/maps/api/js?v=3.exp"></script>
+    <script src="//google-maps-utility-library-v3.googlecode.com/svn/trunk/infobox/src/infobox.js"></script>
+
+    <script>
+        var fpMap = (function (window, locations) {
+
+            var map,
+                markers = {},
+                campusWindow,
+                map_styles = [
+                    {featureType: "road", elementType: "geometry", stylers: [{visibility: "on"}, {lightness: 70}]},
+                    {featureType: "poi", elementType: "geometry", stylers: [{visibility: "off"}]},
+                    {featureType: "landscape", elementType: "geometry", stylers: [{color: "#fffffa"}]},
+                    {featureType: "water", stylers: [{lightness: 0}]},
+                    {featureType: "road", elementType: "labels", stylers: [{visibility: "on"}]},
+                    {featureType: "transit", stylers: [{visibility: "off"}]},
+                    {featureType: "administrative", elementType: "geometry", stylers: [{lightness: 0}]}
+                ];
+
+            google.maps.event.addDomListener(window, 'load', initLocationsMap);
+
+            return {
+                openLocation: openCampusWindow,
+                closeCampusWindow: closeCampusWindow
+            };
+
+            function initLocationsMap() {
+
+                createMap();
+                createInfoWindow();
+                createCampusMarkers();
+
+                google.maps.event.addListener(map, "click", function () {
+                    closeCampusWindow();
+                });
+
+            }
+
+            function createMap() {
+                map = new google.maps.Map(document.getElementById('LocationsMap'), {
+                    zoom: 10,
+                    scrollwheel: false,
+                    center: new google.maps.LatLng(36.0706850, -84.0721176),
+                    styles: map_styles
+                });
+
+                google.maps.event.addListener(map, 'dragend', function() {
+                    console.log(map.getCenter());
+                });
+            }
+
+            function createInfoWindow() {
+                var firstCampus = locations[Object.keys(locations)[0]];
+                campusWindow = new InfoBox({
+                    content: createCampusContent(firstCampus),
+                    alignBottom: true,
+                    infoBoxClearance: new google.maps.Size(20, 40),
+                    enableEventPropagation: false,
+                    maxWidth: 320,
+                    pixelOffset: new google.maps.Size(-160, -55),
+                    disableAutoPan: false,
+                    closeBoxURL: ''
+                });
+                console.log(campusWindow);
+            }
+
+            function createCampusMarkers() {
+                var campus_ident;
+                for (var campus in locations) {
+                    if (locations.hasOwnProperty(campus)) {
+                        campus_ident = locations[campus].ident;
+                        markers[campus_ident] = createMarker(map, locations[campus]);
+                    }
+                }
+            }
+
+            function createMarker(map, campus) {
+
+                var marker = new google.maps.Marker({
+                    map: map,
+                    position: new google.maps.LatLng(campus.lat, campus.lng)
+                });
+
+                google.maps.event.addListener(marker, 'click', function () {
+                    openCampusWindow(campus.ident);
+                });
+
+                return marker;
+
+            }
+
+            function openCampusWindow(campus_ident) {
+
+                var campus = locations[campus_ident],
+                    marker = markers[campus_ident],
+                    content;
+
+                content = createCampusContent(campus);
+
+                console.log('content', content);
+
+                campusWindow.setContent(content);
+                campusWindow.open(map, marker);
+            }
+
+            function closeCampusWindow() {
+                campusWindow.close();
+            }
+
+            function createCampusContent(campus) {
+
+                var content =
+                            '<div class="LocationWindow">' +
+                            '<img class="LocationWindow-photo" src="' + campus.thumbnail + '">' +
+                            '<div class="LocationWindow-body">' +
+                            '<h1 class="LocationWindow-title">' +
+                            campus.title +
+                            '</h1>' +
+                            '<p class="LocationWindow-address">' +
+//              (campus.location.length ? (campus.location + '<br>') : '') +
+                            campus.address + '<br>' +
+                            campus.city + ', ' + campus.state + ' ' + campus.zip + '<br>' +
+                            '<a href="' + campus.directions_url + '">Get Directions</a>' +
+                            '</p>' +
+                            '<h2 class="LocationWindow-subtitle">Service Times</h2>' +
+                            '<p>' +
+                            campus.times.join('<br>') +
+                            '</p>' +
+                            '</div>' +
+                            '<div class="LocationWindow-footer">' +
+                            '<span class="LocationWindow-phone">865-555-1234</span>' +
+                            '<span class="LocationWindow-moreWrap">' +
+                            '<a class="LocationWindow-more" href="' + campus.url + '">More Details</a>' +
+                            '</span>' +
+                            '</div>' +
+                            '<span class="LocationWindow-close" onclick="fpMap.closeCampusWindow();"></span>' +
+                            '<span class="LocationWindow-arrow"></span>'
+                '</div>';
+                return content;
+            }
+
+        }(window, window.fp.locations));
+
+    </script>
 
 @endsection
