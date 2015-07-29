9376d50e60692373c04192f3c412ee8fb809ee3a
Brad Roberts <brad@bradroberts.net>
2015-07-29 18:10:13 -0400

    Updated photo

--- resources/views/fpkids-welcome.blade.php
+++ resources/views/fpkids-welcome.blade.php
@@ -17,8 +17,7 @@
     <p>The best leaders on the planet will engage your kids to connect the Bible to their personal lives through dramas, interactive games, and dynamic worship. They'll learn how to trust God, make wise choices, and treat others the way they want to be treated.</p>
     @endbgsection
 
-            <!-- IMAGE: Need photo for club45 -->
-    @bgsection(['title' => '4th & 5th Grade', 'class' => '', 'image' => 'images/fpkids/prek-wide.jpg'])
+    @bgsection(['title' => '4th & 5th Grade', 'class' => '', 'image' => 'images/fpkids/club-45-wide.jpg'])
     <p>Club 45 is an innovative environment for your preteen. They'll focus on their relationship with Christ as well as their relationship with their peers, parents, and adult leaders through creative teaching, interactive games, and engaging worship.</p>
     @endbgsection
 
@@ -31,7 +30,6 @@
             </div>
         </div>
     </div>
-
 
     @bgsection(['title' => 'Secure Environment', 'class' => 'BackgroundSection--right', 'image' => 'images/fpkids/security-wide.jpg'])
     <p>When you register your child, you'll receive matching identification tags that are exclusive to your family and visit. You'll present this tag when picking up your child. We'll also notify you during the service if your child needs you.</p>
