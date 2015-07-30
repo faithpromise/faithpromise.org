0799a8ccf5adc760fd56eb28df043316dbbbbfea
Brad Roberts <brad@bradroberts.net>
2015-07-29 20:25:19 -0400

    Removed unused function

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -48,14 +48,6 @@ function cdn_image($display_width, $image_width, $image_path, $format = null) {
     return $img_url;
 }
 
-function series_fade_image($series_ident) {
-    $image_path = asset_path('images/series/' . $series_ident . '.jpg');
-    $img_hash = asset_exists($image_path) ? '?v=' . filemtime(asset_path($image_path)) : '';
-    $img_url = config('site.cdn_url') . '/series-fade-out/' . $series_ident . '.jpg' . $img_hash;
-
-    return $img_url;
-}
-
 function bible_verses($scriptures) {
 
     $links = '';
