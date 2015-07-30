d76d50571ab32290370318fa12857cdcf7c4117d
Brad Roberts <brad@bradroberts.net>
2015-07-30 18:00:11 -0400

    Added calendar nav to bottom of page

--- resources/views/events_calendar.blade.php
+++ resources/views/events_calendar.blade.php
@@ -4,18 +4,9 @@
 
     <div class="TextSection TextSection--compact">
         <div class="TextSection-container">
+
             <h2 class="TextSection-title text-center">
-                @if ($allow_prev)
-                    <a class="Calendar-nav" href="{{ $prev_url }}"><i class="icon-left-open-big"></i></a>
-                @else
-                    <span class="Calendar-nav is-disabled"><i class="icon-left-open-big"></i></span>
-                @endif
-                {{ $start->format('F Y') }} Calendar
-                @if ($allow_next)
-                    <a class="Calendar-nav" href="{{ $next_url }}"><i class="icon-right-open-big"></i></a>
-                @else
-                    <span class="Calendar-nav is-disabled"><i class="icon-right-open-big"></i></span>
-                @endif
+                @include('partials.calendar_nav')
             </h2>
 
             @foreach($days as $key => $events)
@@ -37,6 +28,10 @@
                 </table>
 
             @endforeach
+
+            <div class="TextSection-title text-center">
+            @include('partials.calendar_nav')
+            </div>
 
         </div>
     </div>
