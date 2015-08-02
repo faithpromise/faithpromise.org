a0a5976fd96dfbb7e5f4417f2cb58961c82eb684
Brad Roberts <brad@bradroberts.net>
2015-08-01 21:55:21 -0400

    Removed Intervention library.

--- config/app.php
+++ config/app.php
@@ -144,7 +144,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
-        Intervention\Image\ImageServiceProvider::class,
 
     ],
 
@@ -194,7 +193,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'Debugbar'  => Barryvdh\Debugbar\Facade::class,
-        'Image'     => Intervention\Image\Facades\Image::class,
 
     ],
 
