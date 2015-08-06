40b3799eed874e43c4125234fa5414a278faca28
Brad Roberts <brad@bradroberts.net>
2015-08-06 17:48:35 -0400

    Working on sermons page

--- resources/views/partials/hero_video.blade.php
+++ resources/views/partials/hero_video.blade.php
@@ -1,7 +1,7 @@
 <style type="text/css" scoped>
     @media (max-width: 499px) {
         .HeroVideo-image {
-            background-image: url({{ cdn_image('sm', 'full', $video->image) }});
+            background-image: url({{ cdn_image('sm', 'full', $video->image, 'tall') }});
         }
 
         /* -square */
@@ -22,7 +22,65 @@
     }
 </style>
 
-<div class="HeroVideo">
+<div class="HeroVideo2">
+    <div class="HeroVideo2-container">
+
+        @if (! empty($heading))
+            <h2 class="HeroVideo2-heading">{{ $heading }}</h2>
+        @endif
+
+        <div class="HeroVideo-image"></div>
+
+        <h1 class="HeroVideo2-title">{{ $video->title }}</h1>
+
+        <div class="HeroVideo2-meta">
+            @if (strlen($video->speaker_display_name))
+                <span class="HeroVideo2-speaker">{{ $video->speaker_display_name }}</span>
+                <span class="HeroVideo2-metaSeparator">&middot;</span>
+            @endif
+            <span class="HeroVideo-2date">{{ $video->publish_at->format('M d Y') }}</span>
+        </div>
+
+        <div class="HeroVideo2-buttons">
+            @if ($video->vimeo_id)
+                <span class="HeroVideo-button" ng-click="openVideo({{ $video->vimeo_id }})"><i class="icon-play"></i> Watch</span>
+            @endif
+            @if ($video->audio_file)
+                <a class="HeroVideo-button" href="{{ $site['audio_url'] }}{{ $video->audio_file }}" target="_blank">
+                    <i class="icon-headphones"></i>
+                </a>
+            @endif
+            <span class="Dropdown-wrapper" dropdown>
+                <span class="HeroVideo-button" dropdown-toggle>
+                    <i class="icon-dot-3"></i>
+                </span>
+                <div class="Dropdown Dropdown--light Dropdown--bottomLeft">
+                    <ul class="Dropdown-menu">
+                        <li class="Dropdown-item">
+                            <a class="Dropdown-link" href="<?= share_facebook($video->url); ?>">
+                                <i class="icon-facebook"></i> Share on Facebook
+                            </a>
+                        </li>
+                        <li class="Dropdown-item">
+                            <a class="Dropdown-link" href="<?= share_twitter($video->url); ?>">
+                                <i class="icon-twitter"></i> Share on Twitter
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </span>
+            <!--<a class="HeroVideo-button" href="<?= share_facebook($video->url); ?>">
+                <i class="icon-facebook"></i>
+            </a>
+            <a class="HeroVideo-button" href="<?= share_twitter($video->url); ?>">
+                <i class="icon-twitter"></i>
+            </a>-->
+        </div>
+
+    </div>
+</div>
+
+<div class="HeroVideo" style="display: none;">
 
     <div class="HeroVideo-container">
 
@@ -57,12 +115,12 @@
                                 <i class="icon-headphones"></i>
                             </a>
                         @endif
-                            <a class="HeroVideo-button" href="<?= share_facebook($video->url); ?>">
-                                <i class="icon-facebook"></i>
-                            </a>
-                            <a class="HeroVideo-button" href="<?= share_twitter($video->url); ?>">
-                                <i class="icon-twitter"></i>
-                            </a>
+                        <a class="HeroVideo-button" href="<?= share_facebook($video->url); ?>">
+                            <i class="icon-facebook"></i>
+                        </a>
+                        <a class="HeroVideo-button" href="<?= share_twitter($video->url); ?>">
+                            <i class="icon-twitter"></i>
+                        </a>
                     </div>
 
                 </div>
@@ -84,22 +142,22 @@
 
 {{--<div class="HeroVideo-column HeroVideo-column--actions">--}}
 
-    {{--<div class="HeroVideo-actions">--}}
-        {{--<a class="HeroVideo-actionsItem" href="https://www.facebook.com/dialog/share?app_id={{ $site['facebook_app_id'] }}&display=popup&href={{ $video->url }}&redirect_uri={{ $video->url }}">--}}
-            {{--<svg class="HeroVideo-action" role="img" title="Share on Facebook">--}}
-                {{--<use xlink:href="/build/svg/video.svg#facebook"></use>--}}
-            {{--</svg>--}}
-            {{--<span class="HeroVideo-actionText">share</span>--}}
-        {{--</a>--}}
-        {{--<!-- https://dev.twitter.com/web/intents -->--}}
-        {{--<!-- We are using the "Limited Dependencies" code -->--}}
-        {{--<a href="https://twitter.com/intent/tweet?text=Check+it+out&url={{ $video->url }}" class="HeroVideo-actionsItem">--}}
-            {{--<svg class="HeroVideo-action" role="img" title="Tweet it">--}}
-                {{--<use xlink:href="/build/svg/video.svg#twitter"></use>--}}
-            {{--</svg>--}}
-            {{--<span class="HeroVideo-actionText">tweet</span>--}}
-        {{--</a>--}}
+{{--<div class="HeroVideo-actions">--}}
+{{--<a class="HeroVideo-actionsItem" href="https://www.facebook.com/dialog/share?app_id={{ $site['facebook_app_id'] }}&display=popup&href={{ $video->url }}&redirect_uri={{ $video->url }}">--}}
+{{--<svg class="HeroVideo-action" role="img" title="Share on Facebook">--}}
+{{--<use xlink:href="/build/svg/video.svg#facebook"></use>--}}
+{{--</svg>--}}
+{{--<span class="HeroVideo-actionText">share</span>--}}
+{{--</a>--}}
+{{--<!-- https://dev.twitter.com/web/intents -->--}}
+{{--<!-- We are using the "Limited Dependencies" code -->--}}
+{{--<a href="https://twitter.com/intent/tweet?text=Check+it+out&url={{ $video->url }}" class="HeroVideo-actionsItem">--}}
+{{--<svg class="HeroVideo-action" role="img" title="Tweet it">--}}
+{{--<use xlink:href="/build/svg/video.svg#twitter"></use>--}}
+{{--</svg>--}}
+{{--<span class="HeroVideo-actionText">tweet</span>--}}
+{{--</a>--}}
 
-    {{--</div>--}}
+{{--</div>--}}
 
 {{--</div>--}}
