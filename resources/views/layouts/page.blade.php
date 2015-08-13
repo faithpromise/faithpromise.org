b2c13e4697cc1baab14937b1a932cebae6d3e4c5
Brad Roberts <brad@bradroberts.net>
2015-08-13 13:45:48 -0400

    Fixed hero image paths

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,6 +1,5 @@
 <?php
-    $hero_image_wide = isset($hero_image) ? $hero_image : 'images/pages/' . Request::path() . '-wide.jpg';
-    $hero_image_tall = str_replace('-wide.jpg', '-tall.jpg', $hero_image_wide);
+    $hero_image = isset($hero_image) ? $hero_image : 'images/pages/' . Request::path() . '-wide.jpg';
 ?>
 
 @extends('layouts.default')
@@ -9,25 +8,25 @@
 
 <style type="text/css" scoped>
     /* small <= 320px */
-    .hero_image { background-image: url({{ cdn_image('sm', 'full', $hero_image_tall) }}); }
+    .hero_image { background-image: url({{ cdn_image('sm', 'full', $hero_image, 'tall') }}); }
 
     /* medium >= 321px */
     @media (min-width: 20.0625rem) {
-        .hero_image { background-image: url({{ cdn_image('md', 'full', $hero_image_tall) }}); }
+        .hero_image { background-image: url({{ cdn_image('md', 'full', $hero_image, 'tall') }}); }
     }
 
     /* large >= 601px */
     @media (min-width: 30.0625rem) {
-        .hero_image { background-image: url({{ cdn_image('lg', 'full', $hero_image_tall) }}); }
+        .hero_image { background-image: url({{ cdn_image('lg', 'full', $hero_image, 'tall') }}); }
     }
 
     /* x-large >= 481px */
     @media (min-width: 37.5625rem) {
-        .hero_image { background-image: url({{ cdn_image('xl', 'full', $hero_image_tall) }}); }
+        .hero_image { background-image: url({{ cdn_image('xl', 'full', $hero_image, 'tall') }}); }
     }
 
     @media (min-width: 47.5rem) {
-        .hero_image { background-image: url({{ cdn_image('xl', 'full', $hero_image_wide) }}); }
+        .hero_image { background-image: url({{ cdn_image('xl', 'full', $hero_image, 'wide') }}); }
     }
 </style>
 
