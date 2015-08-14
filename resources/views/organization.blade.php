451e5c9299afaa8a80bf28967a7fa66e8a4ac790
Brad Roberts <brad@bradroberts.net>
2015-08-14 14:21:11 -0400

    Added organization view and model

---
+++ resources/views/organization.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.page', ['title' => $organization->name, 'hero_image' => $organization->image])
+
+@section('page')
+
+    @introsection(['title' => $organization->name])
+    {!! $organization->description !!}
+    @endintrosection
+
+    {{--
+        ========================================
+        Contact
+        ========================================
+    --}}
+
+    @include('partials.have_questions', ['class' => 'Section--lightGrey', 'title' => 'Need more info?', 'contact' => $organization->contact, 'email' => $organization->email, 'phone' => $organization->phone, 'website' => $organization->website, 'text' => $organization->more_info])
+
+@endsection
