83849f114d38d612c3cbca2d7e2f06f45bdf5a06
Brad Roberts <brad@bradroberts.net>
2015-08-14 14:21:44 -0400

    Updated have_questions to include website, phone, and contact

--- resources/views/partials/have_questions.blade.php
+++ resources/views/partials/have_questions.blade.php
@@ -2,10 +2,53 @@
 
 $css_class = trim('TextSection TextSection--center ' . (isset($class) ? $class : ''));
 $title = isset($title) ? $title : 'Have Questions?';
+
+$phone = isset($phone) ? $phone : '';
+$website = isset($website) ? $website : '';
 $email = isset($email) ? $email : config('site.email');
-$contact = isset($contact) ? $contact : $email;
-$email_link = '<a href="mailto:' . $email . '">' . $contact . '</a>';
-$text = str_replace('#email#', $email_link, (isset($text) ? $text : 'If you have questions, please contact #email#.'));
+$contact = !empty($contact) ? $contact : $email;
+
+$contact_methods = [];
+
+if (empty($text)) {
+    if (empty($email) && empty($phone) && empty($website)) {
+        return;
+    }
+    if (!empty($email)) {
+        $contact_methods[] = 'email #email#';
+    }
+    if (!empty($phone)) {
+        $contact_methods[] = 'call #phone#';
+    }
+    if (!empty($website)) {
+        $contact_methods[] = 'visit #website#';
+    }
+
+    if (count($contact_methods) > 1) {
+        $contact_methods[count($contact_methods)-1] = 'or ' . $contact_methods[count($contact_methods)-1];
+    }
+
+    $text = 'For more information, please ' . implode(', ', $contact_methods) . '.';
+}
+
+if (strlen($website)) {
+    $website_text = preg_replace('/^http(s?):\/\//', '', $website);
+    $website = '<a href="' . $website . '">' . $website_text . '</a>';
+    $text = str_replace('#website#', $website, $text);
+}
+if (strlen($phone)) {
+    $phone = '<a class="no-wrap" href="tel:' . $phone . '">' . $phone . '</a>';
+    $text = str_replace('#phone#', $phone, $text);
+}
+
+if (strlen($email)) {
+    $email = '<a class="no-wrap" href="mailto:' . $email . '">' . $contact . '</a>';
+    $text = str_replace('#email#', $email, $text);
+}
+
+if (strlen($contact)) {
+    $text = str_replace('#contact#', $contact, $text);
+}
 ?>
 
 <div class="{{ $css_class }}">
