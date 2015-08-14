def13f74b9d3bd6f53bdd1a05cf37dadb0e41009
Brad Roberts <brad@bradroberts.net>
2015-08-14 17:22:43 -0400

    Removed content

--- resources/views/worship.blade.php
+++ resources/views/worship.blade.php
@@ -19,26 +19,6 @@
     @cardsection(['title' => 'Upcoming Events', 'class' => 'Section--lightGrey', 'cards' => $ministry->Events])
     @endcardsection
 
-
-    {{--
-    ================================================================================
-        Serve
-    ================================================================================ --}}
-
-    @bgsection([
-        'title' => 'Join Our Team',
-        'class' => 'get_involved',
-        'image' => 'images/fpkids/rocket-wide.jpg',
-        'buttons' => [
-            [
-                'title' => 'Learn more',
-                'url' => '/worship'
-            ]
-        ]
-    ])
-    <p>We're so glad to have you as our guest at Faith Promise. We want your experience in fpKIDS to be one worth talking about. Please visit our welcome page designed just for you.</p>
-    @endbgsection
-
     {{--
     ================================================================================
         Staff
