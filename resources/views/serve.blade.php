e957a6699f0dfa3b1d506f9e8f5e96f45b13f1b6
Brad Roberts <brad@bradroberts.net>
2015-08-14 15:53:36 -0400

    Added a serve page

--- resources/views/serve.blade.php
+++ resources/views/serve.blade.php
@@ -1,5 +1,15 @@
----
-layout: page
-title: Serve
-permalink: /serve/
----
+@extends('layouts.page', ['title' => 'Serving'])
+
+@section('page')
+
+    @introsection(['title' => 'Opportunities to Serve at Faith Promise'])
+    <p>Ready to take the next step and begin serving at Faith Promise?  Contact us using the form below.</p>
+    @endintrosection
+
+    <div class="Section">
+        <div class="Section-container">
+            <iframe height="613" allowTransparency="true" frameborder="0" scrolling="no" style="width:100%;border:none" src="http://www.faithpromiseweb.com/forms/embed.php?id=11" title="I'm Ready to Volunteer"><a href="http://www.faithpromiseweb.com/forms/view.php?id=11" title="I'm Ready to Volunteer">I'm Ready to Volunteer</a></iframe>
+        </div>
+    </div>
+
+@endsection
