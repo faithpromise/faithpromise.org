d039a2c77bad96dc5fc5356feaa19336d72e52de
Brad Roberts <email@bradroberts.net>
2015-08-15 16:00:04 -0400

    Prevented cards from being resized on mobile

--- js/directives/card-grid.js
+++ js/directives/card-grid.js
@@ -3,41 +3,45 @@
 
     module.directive('cardGrid', directive);
 
-    function directive() {
+    directive.$inject = ['site'];
+
+    function directive(site) {
         return {
             restrict: 'A',
-            link: Link,
-            scope: {}
+            scope: {},
+            link: function (scope, elem) {
+
+                if (site.isMobile) {
+                    return;
+                }
+
+                var i,
+                    paragraph,
+                    heighest = 0,
+                    cardBodies = angular.element(elem).find('div');
+
+                for (i = 0; i < cardBodies.length; i++) {
+
+                    paragraph = angular.element(cardBodies[i]);
+
+                    if (paragraph.hasClass('Card-body')) {
+                        heighest = Math.max(cardBodies[i].offsetHeight, heighest);
+                    }
+
+                }
+
+                for (i = 0; i < cardBodies.length; i++) {
+
+                    paragraph = angular.element(cardBodies[i]);
+
+                    if (paragraph.hasClass('Card-body')) {
+                        paragraph.css('min-height', heighest + 'px');
+                    }
+
+                }
+
+            }
         };
-    }
-
-    function Link(scope, elem) {
-
-        var i,
-            paragraph,
-            heighest = 0,
-            cardBodies = angular.element(elem).find('div');
-
-        for(i = 0; i < cardBodies.length; i++) {
-
-            paragraph = angular.element(cardBodies[i]);
-
-            if (paragraph.hasClass('Card-body')) {
-                heighest = Math.max(cardBodies[i].offsetHeight, heighest);
-            }
-
-        }
-
-        for(i = 0; i < cardBodies.length; i++) {
-
-            paragraph = angular.element(cardBodies[i]);
-
-            if (paragraph.hasClass('Card-body')) {
-                paragraph.css('min-height', heighest + 'px');
-            }
-
-        }
-
     }
 
 })(angular.module('app'));
