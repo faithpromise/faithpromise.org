170c2f1464925c6de875f176a4811102e5d85f0a
Brad Roberts <brad@bradroberts.net>
2015-08-17 15:40:34 -0400

    Fix to ensure same height on cards

--- resources/views/partials/cards.blade.php
+++ resources/views/partials/cards.blade.php
@@ -1,6 +1,6 @@
 <?php
 
-    $card_grid_class = trim('Card-grid ' . (isset($class) ? $class : ''));
+$card_grid_class = trim('Card-grid ' . (isset($class) ? $class : ''));
 
 ?>
 
@@ -8,7 +8,7 @@
     <?php foreach ($cards as $card): ?>
     <li class="Card-item">
         <div class="Card" link-to="<?= isset($card->card_url) ? $card->card_url : ""; ?>">
-            <?php if (isset($card->card_url)): ?>
+            <?php if (!empty($card->card_url)): ?>
             <a class="Card-image b-lazy"
                     href="<?= $card->card_url; ?>"
                     data-src-sm="<?= cdn_image('sm', 'full', $card->card_image) ?>"
@@ -16,29 +16,33 @@
                     data-src-lg="<?= cdn_image('lg', 'full', $card->card_image) ?>"
                     data-src="<?= cdn_image('xl', 'half', $card->card_image) ?>"></a>
             <?php else: ?>
-            <span class="Card-image"
+            <span class="Card-image b-lazy"
                     data-src-sm="<?= cdn_image('sm', 'full', $card->card_image) ?>"
                     data-src-md="<?= cdn_image('md', 'full', $card->card_image) ?>"
                     data-src-lg="<?= cdn_image('lg', 'full', $card->card_image) ?>"
                     data-src="<?= cdn_image('xl', 'half', $card->card_image) ?>"></span>
             <?php endif; ?>
             <div class="Card-body">
-                <?php if (! empty($card->card_url)): ?>
+                <?php if (!empty($card->card_url)): ?>
                 <h3 class="Card-title"><a href="<?= $card->card_url ?>"><?= $card->card_title ?></a></h3>
                 <?php else: ?>
                 <h3 class="Card-title"><?= $card->card_title ?></h3>
                 <?php endif; ?>
-                <?php if (! empty($card->card_subtitle)): ?>
+                <?php if (!empty($card->card_subtitle)): ?>
                 <h4 class="Card-subtitle"><?= $card->card_subtitle ?></h4>
                 <?php endif; ?>
-                <?php if (! empty($card->card_text)): ?>
+                <?php if (!empty($card->card_text)): ?>
                 <p class="Card-text"><?= $card->card_text; ?></p>
                 <?php endif; ?>
             </div>
             <div class="Card-footer">
-                <?php if (isset($card->card_url)): ?>
+                <?php if (!empty($card->card_url)): ?>
                 <a class="Card-link" href="<?= $card->card_url; ?>"><?= (isset($card->card_url_text) && strlen($card->card_url_text)) ? $card->card_url_text : "More Details" ?>
-                    <i class="icon-right-open-big"></i></a>
+                    <i class="icon-right-open-big"></i>
+                </a>
+                <?php else: ?>
+                    <!-- LATER: Need a better solution for ensuring same height on cards. Handle this in card-grid directive. -->
+                    <span class="Card-link">&nbsp;</span>
                 <?php endif; ?>
             </div>
         </div>
