81f7c7217a4b290ce0cbe827436fec695c176c69
Brad Roberts <brad@bradroberts.net>
2015-09-02 11:02:45 -0400

    Building out the volunteer position directive

--- js/main/directives/volunteer-positions/volunteer-positions.js
+++ js/main/directives/volunteer-positions/volunteer-positions.js
@@ -18,6 +18,19 @@
     function Controller($http) {
 
         var vm = this;
+        vm.user = {
+            first_name: '',
+            last_name: '',
+            email: '',
+            phone: '',
+            message: '',
+            campus: '',
+            selected_positions: []
+        };
+        vm.campuses = null;
+        vm.skills = null;
+        vm.toggle_position = toggle_position;
+        vm.is_selected = is_selected;
 
         init();
 
@@ -25,6 +38,60 @@
             $http.get('/serve/opportunities.json').then(function (response) {
                 vm.skills = response.data;
             });
+        }
+
+        function toggle_position(position) {
+
+            var i,
+                idx = -1;
+
+            position.is_selected = !position.is_selected;
+
+            for(i = 0; i < vm.user.selected_positions.length; i++) {
+                if (vm.user.selected_positions[i].id === position.id) {
+                    idx = i;
+                    break;
+                }
+            }
+
+            if (idx >= 0) {
+                vm.user.selected_positions.splice(idx, 1);
+            } else {
+                vm.user.selected_positions.push(position);
+            }
+
+            vm.user.message = build_description();
+
+        }
+
+        function is_selected(position_id) {
+            return vm.user.selected_positions.indexOf(position_id) >= 0;
+        }
+
+        function build_description() {
+            var i,
+                areas = [],
+                areas_string = '';
+
+            if (vm.user.selected_positions.length === 0) {
+                return '';
+            }
+
+            for(i = 0; i < vm.user.selected_positions.length; i++) {
+                areas.push(vm.user.selected_positions[i].title);
+            }
+
+            if (areas.length === 1) {
+                areas_string = areas[0];
+            } else if (areas.length === 2) {
+                areas_string = areas.join(' and ');
+            } else {
+                areas[areas.length - 1] = 'and ' + areas[areas.length - 1];
+                areas_string = areas.join(', ');
+            }
+
+            return 'Please contact me with more information about the ' + areas_string + ' opportunities.';
+
         }
 
     }
