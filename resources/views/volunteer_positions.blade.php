fb81b237b605be330be23308c1edbe3a6d3a553e
Brad Roberts <brad@bradroberts.net>
2015-09-03 10:47:58 -0400

    Updated text

--- resources/views/volunteer_positions.blade.php
+++ resources/views/volunteer_positions.blade.php
@@ -3,7 +3,7 @@
 @section('page')
 
     @introsection(['title' => 'Opportunities to Serve at Faith Promise'])
-    <p>Ready to take the next step and begin serving at Faith Promise?  Contact us using the form below.</p>
+    <p>Ready to take the next step and begin serving at Faith Promise?  Contact us using the form below. Already know where you want to serve? <a href="#connect">Let us know</a>.</p>
     @endintrosection
 
     <volunteer-positions></volunteer-positions>
