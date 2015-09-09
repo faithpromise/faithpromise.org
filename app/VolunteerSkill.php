59f52a553b808522583ebb0330e5351a5ea587e9
Brad Roberts <brad@bradroberts.net>
2015-09-09 11:57:01 -0400

    Renamed relationship on volunteer model

--- app/VolunteerSkill.php
+++ app/VolunteerSkill.php
@@ -9,7 +9,7 @@ class VolunteerSkill extends Model {
     protected $dates = ['created_at', 'updated_at'];
     protected $fillable = ['title', 'description'];
 
-    public function positions() {
+    public function volunteer_positions() {
         return $this->belongsToMany('App\VolunteerPosition', 'volunteer_positions_skills');
     }
 
