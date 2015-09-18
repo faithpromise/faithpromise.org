63143cdaae7fb7e3fb15279b0c6842561faaae74
Brad Roberts <brad@bradroberts.net>
2015-09-18 14:25:02 -0400

    Added phone number and reorganized the location detail page

--- resources/views/locations_detail.blade.php
+++ resources/views/locations_detail.blade.php
@@ -60,17 +60,19 @@ $what_to_expect_cards = collect([
                 ]
             ]
         ])
-            <p>We'd love to see you this weekend at our {{ $campus->name }} Campus located at {{ $campus->address }}, {{ $campus->city }}, {{ $campus->state }} {{ $campus->zip }}. You'll experience contemporary worship and an engaging message from our Senior Pastor Chris Stephens.</p>
+            <p>We'd love to see you this weekend at our {{ $campus->name }} Campus. You'll experience contemporary worship and an engaging message from our Senior Pastor Chris Stephens.</p>
+            <p>Located at {{ $campus->address }}, {{ $campus->city }}, {{ $campus->state }} {{ $campus->zip }}</p>
+            <p>Contact us: (865) 251-2590</p>
             <p><strong>Service times are:</strong> {!! $campus->times !!}.</p>
         @endintrosection
 
     @endif
 
-    @profilessection(['id' => 'staff', 'title' => 'Meet the ' . $campus->name . ' Campus Team', 'class' => 'Section--lightGrey', 'profiles' => $campus->Staff])
-    @endprofilessection
-
-    @cardsection(['title' => 'What to Expect', 'cards' => $what_to_expect_cards])
+    @cardsection(['title' => 'What to Expect', 'class' => 'Section--lightGrey', 'cards' => $what_to_expect_cards])
     <p>When you come, the only thing we ask of you is simple... relax. You're with friends. Whether you've never been to church before or you're a seasoned church veteran, you can anticipate an encouraging and enlightening experience.</p>
     @endcardsection
+
+    @profilessection(['id' => 'staff', 'title' => 'Meet the ' . $campus->name . ' Campus Team', 'profiles' => $campus->Staff])
+    @endprofilessection
 
 @endsection
