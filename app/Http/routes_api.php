68c9c678922c032a22ea3302142aea9ac61760d8
Brad Roberts <brad@bradroberts.net>
2015-09-22 16:04:27 -0400

    Added a videos resource to the API

--- app/Http/routes_api.php
+++ app/Http/routes_api.php
@@ -10,5 +10,6 @@ Route::resource('volunteer-positions', 'Api\VolunteerPositionsController');
 Route::resource('volunteer-position-skills', 'Api\VolunteerPositionSkillsController');
 Route::resource('volunteer-skills', 'Api\VolunteerSkillsController');
 Route::resource('ministries', 'Api\MinistriesController');
+Route::resource('videos', 'Api\VideosController');
 
 });
