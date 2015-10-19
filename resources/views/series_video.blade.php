900b89f81c54d43e641055dede0dd113cad8bbe1
Brad Roberts <brad@bradroberts.net>
2015-10-19 14:42:34 -0400

    Social image on sermons

--- resources/views/series_video.blade.php
+++ resources/views/series_video.blade.php
@@ -1,3 +1,11 @@
+<?php
+
+$og_image = cdn_image('lg', 'full', $video->Series->image, 'tall');
+$og_image_width = isset($og_image_width) ? $og_image_width : '1200';
+$og_image_height = isset($og_image_height) ? $og_image_height : '675';
+
+?>
+
 @extends('layouts.default', ['title' => $video->title, 'hero_image' => $video->Series->image])
 
 @section('content')
