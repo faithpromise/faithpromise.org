9b40c257f9bffc070c30a89d904eec466119f0d6
Brad Roberts <brad@bradroberts.net>
2015-10-20 09:14:03 -0400

    Added Love Starts Here section to missions page

--- resources/views/missions.blade.php
+++ resources/views/missions.blade.php
@@ -15,6 +15,14 @@
     <p>We also engage the world around us, sharing the hope of Christ in tangible ways. Below you will find several opportunities to serve our community.</p>
     @endintrosection
 
+    @bgsection([
+        'title' => 'Love Starts Here',
+        'class' => 'BackgroundSection--right',
+        'image' => 'images/missions/love-starts-here-offset-wide.jpg'
+    ])
+    <p>Good things happen in groups that make serving together a priority. Leaders, download the <a class="no-wrap" href="{{ doc_url('love-starts-here/Love-Starts-Here-Leader-Prep.pdf') }}">Leader Guide</a> and <a class="no-wrap" href="{{ doc_url('love-starts-here/Love-Starts-Here-Group-Discussion.pdf') }}">Group Discussion Guide</a>. When you've decided how you'll serve, please take a moment and <a class="no-wrap" href="http://faithpromiseweb.com/forms/view.php?id=3344">let us know</a>.</p>
+    @endbgsection
+
     @cardsection(['title' => 'Upcoming Trips', 'class' => 'Section--lightGrey', 'cards' => $locations, 'no_text' => true])
     @endcardsection
 
