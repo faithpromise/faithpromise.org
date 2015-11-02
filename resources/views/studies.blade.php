02c29de505036333f485d34e1714804985cf222e
Brad Roberts <brad@bradroberts.net>
2015-11-02 11:56:03 -0500

    Added message to studies page when there are no studies scheduled.

--- resources/views/studies.blade.php
+++ resources/views/studies.blade.php
@@ -2,9 +2,22 @@
 
 @section('page')
 
-    @cardsection(['title' => 'New Short Term Groups', 'cards' => $studies])
-    <p class="text-center">Check out the new opportunities to plug into a small group this fall!</p>
-    @endcardsection
+    @if (empty($studies))
+
+        @introsection(['title' => '', 'class' => '', 'image' => ''])
+        <p>There currently aren't any Bible studies scheduled. However, we plan new studies periodically throughout the year.</p>
+        <p>We'd love for you to <a class="no-wrap" href="{{ route('groups') }}">find a group</a> or attend our
+            <a class="no-wrap" href="/events/next-steps-class">Next Steps Class</a>. You're always welcome to contact us if you have questions or need more information about groups at Faith Promise.
+        </p>
+        @endintrosection
+
+    @else
+
+        @cardsection(['title' => 'New Short Term Studies', 'cards' => $studies])
+        <p class="text-center">Check out the new opportunities to plug into a short term study this fall!</p>
+        @endcardsection
+
+    @endif
 
     @include('partials.have_questions', ['email' => 'jenniferp@faithpromise.org', 'text' => 'If you have questions about a group or ways to get involved, please contact #email#', 'class' => 'Section--lightGrey'])
 
