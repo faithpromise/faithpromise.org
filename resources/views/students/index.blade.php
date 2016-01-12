c32362094e24a628c875fb1db7b014d692e1ad87
Brad Roberts <brad@bradroberts.net>
2016-01-12 15:47:21 -0500

    Start styling index page for fpstudents

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-    <div class="WelcomeHero">
+    <div class="IndexHero">
         {{--<h1 class="">Welcome to Faith Promise</h1>--}}
-        <div class="WelcomeHero-buttons">
-            <a id="to_locations_from_hero" class="WelcomeHero-button" href="{{ route('locations') }}">Locations</a>
-            <a id="to_whatToExpect_from_hero" class="WelcomeHero-button" href="{{ route('whatToExpect') }}">Get to Know Us</a>
+        <div class="IndexHero-buttons">
+            <a id="to_locations_from_hero" class="IndexHero-button" href="{{ route('locations') }}">Locations</a>
+            <a id="to_whatToExpect_from_hero" class="IndexHero-button" href="{{ route('whatToExpect') }}">Get to Know Us</a>
         </div>
     </div>
 
