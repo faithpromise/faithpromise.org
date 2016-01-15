6fba18ff88afe0e8643e1e270e0af20d155ff294
Brad Roberts <brad@bradroberts.net>
2016-01-15 14:08:52 -0500

    File cleanup

--- app/Http/Controllers/Students/MainController.php
+++ app/Http/Controllers/Students/MainController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\Students;
 
+use Illuminate\Http\Request;
 use \Illuminate\Routing\Controller as BaseController;
-use \Illuminate\Support\Facades\Route;
 
 class MainController extends BaseController {
 
-    public function index() {
+    public function index(Request $request) {
         return view('students/index');
     }
 
