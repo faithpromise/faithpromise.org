8f5d08475713a702cab6a70d19cbed192ed55316
Brad Roberts <brad@bradroberts.net>
2016-01-25 10:13:33 -0500

    Passing current series to students home page

--- app/Http/Controllers/Students/MainController.php
+++ app/Http/Controllers/Students/MainController.php
@@ -2,13 +2,19 @@
 
 namespace App\Http\Controllers\Students;
 
+use FaithPromise\Shared\Models\Series;
 use Illuminate\Http\Request;
 use \Illuminate\Routing\Controller as BaseController;
 
 class MainController extends BaseController {
 
     public function index(Request $request) {
-        return view('students/index');
+
+        $current_series = Series::currentSeries()->first();
+
+        return view('students/index', [
+            'current_series' => $current_series
+        ]);
     }
 
 }
