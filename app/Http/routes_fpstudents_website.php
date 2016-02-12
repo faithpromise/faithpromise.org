06f4dce6496491c4ea0f60ee54451deee156bb8d
Brad Roberts <brad@bradroberts.net>
2016-02-12 15:08:35 -0500

    Updated student site routes

--- app/Http/routes_fpstudents_website.php
+++ app/Http/routes_fpstudents_website.php
@@ -9,12 +9,21 @@ Route::group(['domain' => config('site.students_domain')], function () {
     Route::get('/leaders', 'Students\MainController@index');
 
     Route::get('/parents', ['as' => 'fpStudents_parents', 'uses' => 'Students\ParentsController@index']);
+    Route::get('/parents/resources', ['as' => 'fpStudents_parentResources', 'uses' => 'Students\ParentsController@resources']);
 
-    Route::get('/serve', ['as' => 'fpStudnets_serve', 'uses' => 'Students\MainController@serve']);
+    Route::get('/staff', ['as' => 'fpStudents_staff', 'uses' => 'Students\StaffController@staff']);
+    Route::get('/staff/{staff}', ['as' => 'fpStudents_staffDetail', 'uses' => 'Students\StaffController@detail']);
 
-    Route::get('/students', 'Students\MainController@index');
+    Route::get('/locations', ['as' => 'fpStudents_locations', 'uses' => 'Students\CampusesController@index']);
+    Route::get('/locations/{campus}', ['as' => 'fpStudents_location', 'uses' => 'Students\CampusesController@detail']);
 
-    Route::get('/sermons', 'Students\MainController@index');
+    Route::get('/events', 'Students\UpdatesController@events');
+    Route::get('/updates', ['as' => 'fpStudents_updates', 'uses' => 'Students\UpdatesController@index']);
+
+// Sermons
+    Route::get('/sermons', ['as' => 'fpStudents_sermons', 'uses' => 'Students\SermonsController@index']);
+    Route::get('/series/{series}', ['as' => 'fpStudents_series', 'uses' => 'Students\SermonsController@series']);
+    Route::get('/series/{series}/{series_video}', ['as' => 'fpStudents_seriesVideo', 'uses' => 'Students\SermonsController@video']);
 
     Route::any('{all}', 'Students\MainController@index');
 
