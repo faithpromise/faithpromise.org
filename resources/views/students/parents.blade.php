6e27b65c5ecae8fa5769547ed505a77f05f0e1ee
Brad Roberts <brad@bradroberts.net>
2016-02-12 15:09:04 -0500

    Updated parents and parent resources pages on student site

--- resources/views/students/parents.blade.php
+++ resources/views/students/parents.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.page', ['title' => 'Parent Guide'])
+@extends('layouts.default', ['title' => 'Welcome Parents'])
 
-@section('page')
+@section('content')
 
     {{--
     ================================================================================
@@ -8,42 +8,61 @@
     ================================================================================ --}}
 
     @introsection(['title' => 'Welcome Parents'])
-    <p>fpStudents realizes that we are not the answer to discipling students…parents are! We are excited to get to partner with parents in ministering to their students, even in the midst of changing dynamics in their relationship with their students. The following is a list of resources that we encourage you as a parent to utilize to continue connecting with your student and help them grow in their faith.</p>
+    <p>fpStudents realizes that we are not the answer to discipling students... parents are! We are excited to get to partner with parents in ministering to their students, even in the midst of changing dynamics in their relationship with their students.</p>
     <!-- TODO: Add social links -->
     @endintrosection
 
     @bgsection(
         [
-            'title' => 'The Parent Resource',
-            'image' => 'images/fpstudents/worship-2-wide.jpg',
+            'title' => 'Parent Resources',
+            'image' => 'images/general/steps-wide.jpg',
             'buttons' => [
                 [
-                    'title' => 'Check it Out',
-                    'url' => 'http://fpstudents.tumblr.com/'
+                    'title' => 'View Resources',
+                    'url' => route('fpStudents_parentResources')
                 ]
             ]
         ]
     )
-    <p>With each series we provide questions and ideas to spark discussion with your students that correspond with our weekly services.</p>
+    {{--<p>With each series we provide questions and ideas to spark discussion with your students that correspond with our weekly services.</p>--}}
+    <p>We encourage you as a parent to take advantage of these resources to connect with your student and help them grow in their faith.</p>
     @endbgsection
 
-    {{--
-    ================================================================================
-        Serve
-    ================================================================================ --}}
-
-    @bgsection(['title' => 'Get Involved', 'image' => 'images/fpstudents/worship-2-wide.jpg'])
-            <!-- TODO: Update text -->
-    <p></p>
-    <p><a class="Button" href="http://fpstudents.com">Learn more</a></p>
-    @endbgsection
 
     {{--
     ================================================================================
         Staff
     ================================================================================ --}}
 
-    {{--@profilessection(['title' => 'Meet the fpStudents Staff', 'profiles' => $ministry->Staff])--}}
-    {{--@endprofilessection--}}
+    @profilessection(['title' => 'Meet the fpStudents Staff', 'profiles' => $staff])
+    @endprofilessection
+
+    {{--
+    ================================================================================
+        Serve
+    ================================================================================ --}}
+
+    @bgsection(
+        [
+            'title' => 'Get Involved',
+            'class' => 'BackgroundSection--right',
+            'image' => 'images/fpstudents/worship-wide.jpg',
+            'buttons' => [
+                [
+                    'title' => 'Contact Jeff',
+                    'url' => route('fpStudents_staff', 'jeff-cochran')
+                ]
+            ]
+        ])
+            <!-- TODO: Update text -->
+    <p>There's no better way to tell your student how important church is than by getting involved. You can support our students as a greeter, snack volunteer, crowd control, group leader, tech, and more.</p>
+    @endbgsection
+
+    {{--
+        ========================================
+        Contact
+        ========================================
+    --}}
+    @include('partials.have_questions', ['email' => 'fpsglobal@faithpromise.org', 'text' => 'If you have questions we\'d love to hear from you. Please contact us at #email#'])
 
 @endsection
