62323dee19eabe4c8fc7b228fc5a8ed78b6694d4
Brad Roberts <brad@bradroberts.net>
2016-02-18 15:17:36 -0500

    Updated Postgres migration to reset auto increment ids

--- database/seeds/PostgresSeeder.php
+++ database/seeds/PostgresSeeder.php
@@ -12,42 +12,45 @@ class PostgresSeeder extends Seeder {
     public function run() {
 
         $tables = [
-            'bible_plan',
-            'calendar_events',
-            'campuses',
-            'ministries',
-            'mission_locations',
-            'mission_trips',
-            'missionaries',
-            'organizations',
-            'post_locations',
-            'posts',
-            'series',
-            'staff',
-            'staff_ministry',
-            'staff_team',
-            'studies',
-            'study_times',
-            'teams',
-            'ticket_requirements',
-            'ticket_tasks',
-            'users',
-            'videos',
-            'volunteer_positions',
-            'volunteer_positions_skills',
-            'volunteer_skills'];
+            'bible_plan' => 1,
+            'calendar_events' => 0,
+            'campuses' => 1,
+            'ministries' => 1,
+            'mission_locations' => 1,
+            'mission_trips' => 1,
+            'missionaries' => 1,
+            'organizations' => 1,
+            'post_locations' => 0,
+            'posts' => 1,
+            'series' => 1,
+            'staff' => 1,
+            'staff_ministry' => 1,
+            'staff_team' => 1,
+            'studies' => 1,
+            'study_times' => 1,
+            'teams' => 1,
+            'ticket_requirements' => 1,
+            'ticket_tasks' => 1,
+            'users' => 1,
+            'videos' => 1,
+            'volunteer_positions' => 1,
+            'volunteer_positions_skills' => 0,
+            'volunteer_skills' => 1
+        ];
 
-        foreach ($tables as $table) {
+        foreach ($tables as $table => $incrementing) {
 
             DB::connection('pgsql')->table($table)->truncate();
 
             $records = DB::connection('mysql')->table($table)->get();
             foreach ($records as $record) {
                 $input = (array)$record;
-                if (array_key_exists('id', $input) && !in_array($table, ['calendar_events'])) {
-                    unset($input['id']);
-                }
                 DB::connection('pgsql')->table($table)->insert($input);
+            }
+
+            if ($incrementing) {
+                $max_id = max(intval(DB::table($table)->max('id')), 0) + 1;
+                DB::connection('pgsql')->select('ALTER SEQUENCE ' . $table . '_id_seq RESTART WITH ' . $max_id);
             }
 
         }
