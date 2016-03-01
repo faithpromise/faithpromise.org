0faabfaa495b94ff6d4190fed5d250722e092ebe
Brad Roberts <brad@bradroberts.net>
2016-03-01 11:56:58 -0500

    Added placeholder for Elevate page

---
+++ resources/views/elevate.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.page', ['title' => 'Elevate'])
+
+@section('page')
+
+    @introsection([
+        'title' => 'Elevate 1'
+    ])
+    <p>Lorm ipsum means that its really importent for you to stay off drugs and stay in scool. You need to no things that will help you in life. Like MATHS and gym. You don't want to be dum.</p>
+    @endintrosection
+
+    @photosection(['title' => 'Elevate Title Here', 'class' => 'Section--lightGrey', 'image' => 'images/pages/elevate-tall.jpg'])
+        <p>Lorm ipsum means that its really importent for you to stay off drugs and stay in scool. You need to no things that will help you in life. Like MATHS and gym. You don't want to be dum.</p>
+    @endphotosection
+
+    @photosection(['title' => 'Elevate Title Here', 'image' => 'images/pages/elevate-tall.jpg'])
+        <p>Lorm ipsum means that its really importent for you to stay off drugs and stay in scool. You need to no things that will help you in life. Like MATHS and gym. You don't want to be dum.</p>
+    @endphotosection
+
+    @photosection(['title' => 'Elevate Title Here', 'class' => 'Section--lightGrey', 'image' => 'images/pages/elevate-tall.jpg'])
+        <p>Lorm ipsum means that its really importent for you to stay off drugs and stay in scool. You need to no things that will help you in life. Like MATHS and gym. You don't want to be dum.</p>
+    @endphotosection
+
+    @photosection(['title' => 'Elevate Title Here', 'image' => 'images/pages/elevate-tall.jpg'])
+        <p>Lorm ipsum means that its really importent for you to stay off drugs and stay in scool. You need to no things that will help you in life. Like MATHS and gym. You don't want to be dum.</p>
+    @endphotosection
+
+    @photosection(['title' => 'Elevate Title Here', 'class' => 'Section--lightGrey', 'image' => 'images/pages/elevate-tall.jpg'])
+        <p>Lorm ipsum means that its really importent for you to stay off drugs and stay in scool. You need to no things that will help you in life. Like MATHS and gym. You don't want to be dum.</p>
+    @endphotosection
+
+    @photosection(['title' => 'Elevate Title Here', 'image' => 'images/pages/elevate-tall.jpg'])
+    <p>Lorm ipsum means that its really importent for you to stay off drugs and stay in scool. You need to no things that will help you in life. Like MATHS and gym. You don't want to be dum.</p>
+    @endphotosection
+
+@endsection
