bb80bde205286491de9d420c86b003062cb13eed
Brad Roberts <brad@bradroberts.net>
2016-03-01 11:44:37 -0500

    Added button to sign up for Infuse

--- resources/views/infuse.blade.php
+++ resources/views/infuse.blade.php
@@ -4,7 +4,13 @@
 
     @videosection([
         'title' => 'Infuse',
-        'video' => '154897722'
+        'video' => '154897722',
+        'buttons' => [
+            [
+                'title' => 'I\'m Interested!',
+                'url' => 'http://goo.gl/forms/atAlujme0m'
+            ]
+        ]
     ])
     <p>Infuse is a very intentional process that is more than just equipping people with talent to be a part of fpWorship. It is an atmosphere that promotes growth opportunities for Infusers as well as current staff/volunteers. It is important for us to be reminded that Jesus is our ultimate source and without him, we cannot do this process or anything else. We are trying to in turn, remind Infusers and each other that it’s God’s anointing through our talent that connects and changes lives.</p>
     @endvideosection
