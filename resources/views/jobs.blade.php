a029d3281cecf9f505706544c8fba7327eb24946
Brad Roberts <brad@bradroberts.net>
2016-03-01 11:45:47 -0500

    Added default message on jobs page when there are no openings.

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -8,7 +8,7 @@ $childcare->card_url = doc_url('jobs/childcare-team-member.pdf');
 $childcare->card_url_text = 'Download PDF';
 $childcare->card_link_id = 'to_childcare_job_from_card';
 
-$openings = collect([$childcare]);
+$openings = collect([]);
 
 ?>
 
@@ -19,12 +19,16 @@ $openings = collect([$childcare]);
     @introsection([
         'title' => 'Join Our Team'
     ])
-    <p>Interested in joining the staff team of the fastest-growing church in East Tennessee? See an opportunity below where you could put your expertise to work to help move our church to the next level? If so, we'd love to hear from you.</p>
+    @if ($openings->count())
+        <p>Interested in joining the staff team of the fastest-growing church in East Tennessee? See an opportunity below where you could put your expertise to work to help move our church to the next level? If so, we'd love to hear from you.</p>
+    @else
+        <p>There are currently no openings at this time, but check back again because we are always looking for great talent!</p>
+    @endif
     @endintrosection
 
     @cardsection(['title' => 'Current Opportunities', 'class' => 'Section--lightGrey', 'cards' => $openings])
     @endcardsection
 
-    @include('partials.have_questions', ['email' => 'mallorye@faithpromise.org', 'contact' => 'Mallory Ellis', 'text' => 'If you have questions about employment opportunities at Faith Promise, please contact #email#'])
+    @include('partials.have_questions', ['email' => 'abbyz@faithpromise.org', 'contact' => 'Abby Ziegenfuss', 'text' => 'If you have questions about employment opportunities at Faith Promise, please contact #email#'])
 
 @endsection
