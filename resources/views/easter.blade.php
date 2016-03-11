61f5e2f12b0a1ff8ce10a82258895f7ea6d8fc7a
Brad Roberts <brad@bradroberts.net>
2016-03-11 12:31:02 -0500

    Easter page updates

--- resources/views/easter.blade.php
+++ resources/views/easter.blade.php
@@ -4,9 +4,11 @@ function formatted_service_times($times) {
     return '<span class="no-wrap">' . implode('</span>, <span class="no-wrap">', $times) . '</span>';
 }
 
+$og_image = url('images/series/after-the-fall-tall.jpg');
+
 ?>
 
-@extends('layouts.default', ['title' => 'Easter'])
+@extends('layouts.default', ['title' => 'Easter', 'og_image' => $og_image])
 
 @section('content')
 
@@ -14,7 +16,7 @@ function formatted_service_times($times) {
         <div class="AfterTheFall-content">
             <div class="AfterTheFall-videoContainer">
                 <div class="VideoPlayer">
-                    <iframe src="https://player.vimeo.com/video/157875912?title=0&byline=0&portrait=0&badge=0&autoplay=false" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+                    <iframe src="https://player.vimeo.com/video/158649055?title=0&byline=0&portrait=0&badge=0&autoplay=false" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
                 </div>
             </div>
             <div class="AfterTheFall-shareContainer">
@@ -29,14 +31,6 @@ function formatted_service_times($times) {
             </div>
         </div>
     </div>
-
-    {{--@videosection(['title' => 'What would you risk to save a friend?', 'class' => '', 'video' => '157875912'])--}}
-    {{--<p>Based on true events, After The Fall follows the story of a man who risks it all to save his friend when a hunting trip in Montana turns tragic. Degrees separating life and death, three families pray that God will show up to save these men from a deadly storm.</p>--}}
-    {{--@endvideosection--}}
-
-    {{--@cardsection(['title' => 'Find a Location', 'cards' => $campuses, 'class' => 'GridSection--center Section--lightGrey'])--}}
-    {{--<p>Our Faith Promise original film, &quot;After the Fall&quot; will be shown at all of our locations during the week leading up to Easter, and Easter weekend. Choose a location below for specific times and directions.</p>--}}
-    {{--@endcardsection--}}
 
     <div class="Section Section--lightGrey">
         <div class="Section-container">
