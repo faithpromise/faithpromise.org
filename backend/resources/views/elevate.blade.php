0bd6f215fbcc9406cdca3ddd908f636d3fe923bd
Brad Roberts <brad@bradroberts.net>
2016-03-30 07:51:06 -0400

    Updated text

--- backend/resources/views/elevate.blade.php
+++ backend/resources/views/elevate.blade.php
@@ -5,7 +5,8 @@
     @introsection([
         'title' => 'Elevate 1'
     ])
-    <p>A description of Elevate here. Bacon ipsum dolor amet turducken shoulder leberkas spare ribs filet mignon porchetta ribeye swine beef ribs kevin. Andouille cow frankfurter pig pancetta. Ball tip biltong tenderloin pork loin.</p>
+    <p>Welcome to Elevate! Thank you for your investment and commitment to growing as a leader. Elevate is a web-based leadership development tool designed to help all of us who lead, or would like to lead at Faith Promise increase our leadership capacity. As my friend Bill Hybels says, &quot;everyone wins when a leader gets better&quot;. I can't wait to see your kingdom impact as you take your leadership to the next level.</p>
+    <p>Pastor Chris</p>
     @endintrosection
 
     @foreach($elevate_lessons as $lesson)
