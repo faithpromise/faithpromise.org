0b141257d747f2b432f73b61714c9508a6570c53
Brad Roberts <brad@bradroberts.net>
2016-03-31 08:47:57 -0400

    Updated 404 text

--- backend/resources/views/errors/404.blade.php
+++ backend/resources/views/errors/404.blade.php
@@ -3,7 +3,7 @@
 @section('page')
 
     @introsection(['title' => 'Oh no!'])
-        <p>We couldn't find the page you requested. It could have been moved or deleted. Please try using the navigation above to find the information you need.</p>
+        <p>The page you requested does not exist. Please try using the navigation above to find the information you need.</p>
     @endintrosection
 
     @include('partials.have_questions', ['text' => 'If you can\'t find what you need, please contact us at #email# so we can help out.', 'class' => 'Section--lightGrey'])
