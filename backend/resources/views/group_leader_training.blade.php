c472b02e5be56d3095e9607ad8520854349293b5
Brad Roberts <brad@bradroberts.net>
2016-10-04 09:17:30 -0400

    Removed copy pasta

--- backend/resources/views/group_leader_training.blade.php
+++ backend/resources/views/group_leader_training.blade.php
@@ -1,22 +1,3 @@
-<?php
-
-$faq = collect([
-        (object)[
-                'q' => 'Post Attendance &amp; Manage Groups',
-                'a' => '<p>Login here to post your attendance, manage contacts, and send updates to your group. <a href="https://fpctystn.infellowship.com/UserLogin/Index?ReturnUrl=/" target="_blank">Click here</a>.</p>'
-        ],
-        (object)[
-                'q' => 'Discussion Questions',
-                'a' => '<p>Weekly discussion questions to take the sermon message to the group setting. <a href="http://blog.faithpromise.org/category/groups-ministry/discussion-questions/" target="_blank" target="_blank">Discussion Questions</a>.</p>'
-        ],
-        (object)[
-                'q' => 'Group Studies from RightNow',
-                'a' => '<p><a href="http://training.rightnow.org/Training/SignUp/Login.aspx" target="_blank">Login to online video curriculum</a></p><p><a href ="http://support.rightnow.org/forums/20207511-frequently-asked-questions" target="_blank">Frequently asked questions about the video curriculum</a></p>'
-        ]
-]);
-
-?>
-
 @extends('layouts.page', ['title' => 'Group Leader Training', 'hero_image' => 'images/pages/group-leader-training-wide.jpg'])
 
 @section('page')
