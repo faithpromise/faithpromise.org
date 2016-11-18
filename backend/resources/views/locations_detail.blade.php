1fadbad5dd7183739edb80a5671e8757635dd6d3
Brad Roberts <brad@bradroberts.net>
2016-11-18 13:17:44 -0500

    Added New Years times to location page

--- backend/resources/views/locations_detail.blade.php
+++ backend/resources/views/locations_detail.blade.php
@@ -4,6 +4,7 @@
 
 $easter_times = $campus->easter_times;
 $christmas_times = $campus->christmas_times;
+$new_years_times = $campus->new_years_times;
 
 ?>
 
@@ -17,6 +18,7 @@ $christmas_times = $campus->christmas_times;
             <div class="HolidayTimesSection-container">
                 <h2 class="HolidayTimesSection-title">Easter Service Times at {{ $campus->name }}</h2>
                 <h3 class="HolidayTimesSection-campus">{{ $campus->full_name }}</h3>
+
                 {{--<p class="HolidayTimesSection-text">We're excited for you to visit one of our Easter Services.</p>--}}
                 <ul class="HolidayTimesSection-schedule">
                     @foreach($easter_times as $time)
@@ -33,23 +35,37 @@ $christmas_times = $campus->christmas_times;
         </div>
     @endif
 
-    @if ($christmas_times)
-        <div id="christmas-times" class="HolidayTimesSection">
+    @if ($christmas_times || $new_years_times)
+        <div id="christmas-times" class="HolidayTimesSection HolidayTimesSection--christmas">
             <img class="HolidayTimesSection-campusPhoto" src="{{ resized_image_url($campus->image, 320, 'square') }}">
             <div class="HolidayTimesSection-container">
-                <h2 class="HolidayTimesSection-title">Christmas Service Times</h2>
+                <h2 class="HolidayTimesSection-title">Christmas @if ($new_years_times) and New Years @endif Service Times</h2>
                 <h3 class="HolidayTimesSection-campus">{{ $campus->full_name }}</h3>
+                <p class="HolidayTimesSection-text">
+                    * fpKids will serve Pre-K only during all Christmas service times.
+                </p>
                 {{--<p class="HolidayTimesSection-text">We're excited for you to visit one of our Christmas Services.</p>--}}
                 <ul class="HolidayTimesSection-schedule">
-                    @foreach($christmas_times as $time)
-                        <li class="HolidayTimesSection-service">
-                            <span class="HolidayTimesSection-day">{!! $time->day !!}</span><br>
-                            <span class="HolidayTimesSection-time">{!! $time->formatted_times !!}</span>
-                        </li>
-                    @endforeach
+                    @if ($christmas_times)
+                        @foreach($christmas_times as $time)
+                            <li class="HolidayTimesSection-service">
+                                <span class="HolidayTimesSection-day">{!! $time->day !!} *</span><br>
+                                <span class="HolidayTimesSection-time">{!! $time->formatted_times !!}</span>
+                            </li>
+                        @endforeach
+                    @endif
+                    @if ($new_years_times)
+                        @foreach($new_years_times as $time)
+                            <li class="HolidayTimesSection-service">
+                                <span class="HolidayTimesSection-day">{!! $time->day !!}</span><br>
+                                <span class="HolidayTimesSection-time">{!! $time->formatted_times !!}</span>
+                            </li>
+                        @endforeach
+                    @endif
                 </ul>
             </div>
         </div>
+
     @endif
 
     @if($campus->slug === 'online')
@@ -64,17 +80,18 @@ $christmas_times = $campus->christmas_times;
                 ]
             ]
         ])
-            <p>We'd love to see you this weekend on our Internet Campus. You'll experience dynamic worship, an engaging message, and live chat.</p>
-            <strong>Regular service times are:</strong><br>
-            @foreach($campus->times as $time)
-                <span class="ServiceTimes-day">{!! $time->day !!}</span> at <span class="ServiceTimes-time">{!! $time->formatted_times !!}</span><br>
-            @endforeach
-            @if($christmas_times)
-                <p>(See below for special <a href="#christmas-times">Christmas times</a>)</p>
-            @endif
-            @if($easter_times)
-                <p>(See below for special <a href="#easter-times">Easter times</a>)</p>
-            @endif
+        <p>We'd love to see you this weekend on our Internet Campus. You'll experience dynamic worship, an engaging message, and live chat.</p>
+        <strong>Regular service times are:</strong><br>
+        @foreach($campus->times as $time)
+            <span class="ServiceTimes-day">{!! $time->day !!}</span> at
+            <span class="ServiceTimes-time">{!! $time->formatted_times !!}</span><br>
+        @endforeach
+        @if($christmas_times)
+            <p>(See below for special <a href="#christmas-times">Christmas times</a>)</p>
+        @endif
+        @if($easter_times)
+            <p>(See below for special <a href="#easter-times">Easter times</a>)</p>
+        @endif
         @endintrosection
 
     @else
@@ -90,21 +107,22 @@ $christmas_times = $campus->christmas_times;
                 ]
             ]
         ])
-            <p>We'd love to see you this weekend at our {{ $campus->full_name }}. You'll experience contemporary worship and an engaging message from our Senior Pastor Chris Stephens.</p>
-            <p>Located at {{ $campus->address }}, {{ $campus->city }}, {{ $campus->state }} {{ $campus->zip }}</p>
-            <p>Contact us: (865) 251-2590</p>
-            <p>
+        <p>We'd love to see you this weekend at our {{ $campus->full_name }}. You'll experience contemporary worship and an engaging message from our Senior Pastor Chris Stephens.</p>
+        <p>Located at {{ $campus->address }}, {{ $campus->city }}, {{ $campus->state }} {{ $campus->zip }}</p>
+        <p>Contact us: (865) 251-2590</p>
+        <p>
             <strong>Regular service times are:</strong><br>
             @foreach($campus->times as $time)
-                <span class="ServiceTimes-day">{!! $time->day !!}</span> at <span class="ServiceTimes-time">{!! $time->formatted_times !!}</span><br>
+                <span class="ServiceTimes-day">{!! $time->day !!}</span> at
+                <span class="ServiceTimes-time">{!! $time->formatted_times !!}</span><br>
             @endforeach
-            </p>
-            @if($christmas_times)
-                <p>(See below for special <a href="#christmas-times">Christmas times</a>)</p>
-            @endif
-            @if($easter_times)
-                <p>(See below for special <a href="#easter-times">Easter times</a>)</p>
-            @endif
+        </p>
+        @if($christmas_times)
+            <p>(See below for special <a href="#christmas-times">Christmas times</a>)</p>
+        @endif
+        @if($easter_times)
+            <p>(See below for special <a href="#easter-times">Easter times</a>)</p>
+        @endif
         @endintrosection
 
     @endif
