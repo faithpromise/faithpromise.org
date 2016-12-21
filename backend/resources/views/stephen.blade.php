79f885a409882a7176ab808ffef3d5adbb331232
Brad Roberts <brad@bradroberts.net>
2016-12-21 13:49:55 -0500

    Stephen min changes

--- backend/resources/views/stephen.blade.php
+++ backend/resources/views/stephen.blade.php
@@ -12,15 +12,15 @@
     <p>The Faith Promise Stephen Ministry equips lay people to provide confidential, one-on-one, Christ-centered care and support to people experiencing difficult times in their lives such as loss of a loved one, job loss, terminal illness, and divorce. The ministry pairs individuals with a Stephen Minister who will come alongside them to provide emotional and spiritual care for as long as the need persists.</p>
     <p>Stephen Ministers are selected, trained, and supervised to provide effective Christian care. After an initial fifty hours of training, Stephen Ministers are assigned a person with whom they meet once a week for about an hour. Stephen Ministers also meet twice per month for supervision, support, and continuing education.</p>
 
-    <p><strong>Hurting?</strong> <a href="https://docs.google.com/document/d/1ag_OAK7EdGplssIikEhePgqzDcTzzizlmOSvnbntFV4/edit?pli=1">Request a Stephen Minister</a></p>
+    <p><strong>Hurting?</strong> <a target="_blank" href="https://docs.google.com/document/d/1ouxfTSd3YvB76tgIrXvCAeisMQZegBZXULV-s4tjc98/edit?usp=sharing">Request a Stephen Minister</a></p>
 
-    <p><strong>Gifted to help others?</strong> <a href="{{ doc_url('stephen-minister-interest-sheet.pdf') }}">Become a Stephen Minister</a></p>
+    <p><strong>Gifted to help others?</strong> <a target="_blank" href="https://docs.google.com/document/d/1oRgKg-zWw1bgTRgjuh5zJ8ZeY1_G9F6kQBg8wlfBBUo/edit?usp=sharing">Become a Stephen Minister</a></p>
 
     <h5>Additional Resources</h5>
     <ul>
         <li><a href="http://blog.faithpromise.org/category/groups-ministry/stephen-ministry/">Blog &amp; Updates</a></li>
-        <li><a href="https://docs.google.com/document/d/1mjO5Vegv8R5C1ZJUA7UNSF_8SBy3dGs8IJSyqMxLsEo/edit?pli=1">Testimonials &amp; Articles</a></li>
-        <li><a href="https://docs.google.com/document/d/1SDV_kcKghGyzAld5Y28rVbl1wgaNy6iKj6SQkk6TF98/edit?pli=1">Stephen Ministry Resources</a></li>
+        <li><a href="https://docs.google.com/document/d/1wzwsULTF0c650-IMuGxMoKAoZA1NJdHMgGluxtwzLYM/edit?usp=sharing">Testimonials &amp; Articles</a></li>
+        <li><a href="https://docs.google.com/document/d/1lkUybCHy3pS-sH4xqGn3yRRgH6HdBBlRGuGdAF0KuzU/edit?usp=sharing">Stephen Ministry Resources</a></li>
     </ul>
     @endintrosection
 
