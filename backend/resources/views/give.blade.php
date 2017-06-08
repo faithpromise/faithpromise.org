9ba2358d2556b57d56c8f69cac335289f67dfad9
Brad Roberts <brad@bradroberts.net>
2017-06-08 15:36:47 -0400

    Updated financial link

--- backend/resources/views/give.blade.php
+++ backend/resources/views/give.blade.php
@@ -76,7 +76,7 @@ $ways_to_give = collect([
             </div>
             <div class="FinAccountability-text">
                 <p class="FinAccountability-info">Faith Promise Church is accredited by the Evangelical Council for Financial Accountability. Founded in 1979, ECFA provides accreditation to leading Christian nonprofit organizations that faithfully demonstrate compliance with established standards for financial accountability, transparency, fundraising, and board governance.</p>
-                <a class="FinAccountability-link" href="https://www.dropbox.com/sh/hxgbgmb1n90iiz0/AAAkE3UvzhexANAg3lcMBekfa?dl=0">Yearly Audited &amp; Financial Statements</a>
+                <a class="FinAccountability-link" href="https://www.dropbox.com/sh/f2emere264iuxrn/AAD9zHe6iSCBEdd8VLlqHMVNa?dl=0&lst=">Yearly Audited &amp; Financial Statements</a>
             </div>
         </div>
     </div>
